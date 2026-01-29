cacf4ec156a9ea6061de6377909c8b271a12a795
Ph-Tejas <sharmatejas2005@gmail.com>
2026-01-29 23:03:57 +0530

    Time: 2222 ms (26.02%) | Memory: 326.6 MB (40.65%) - LeetSync

---
+++ 3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int findp(vector<int>& p, int x) {
+        if (p[x] == x) return x;
+        return p[x] = findp(p, p[x]);
+    }
+
+    int minCost(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        int N = n * m;
+
+        vector<vector<int>> vis(N, vector<int>(k + 1, 0));
+
+        vector<pair<int,int>> cells(N);
+        for (int i = 0; i < N; i++)
+            cells[i] = {grid[i / m][i % m], i};
+        sort(cells.begin(), cells.end());
+
+        vector<vector<int>> nextPos(k);
+        for (int t = 0; t < k; t++) {
+            nextPos[t].resize(N + 1);
+            for (int i = 0; i <= N; i++) nextPos[t][i] = i;
+        }
+
+        priority_queue<
+            array<int,3>,
+            vector<array<int,3>>,
+            greater<array<int,3>>
+        > pq;
+
+        pq.push({0, N - 1, k});
+
+        while (!pq.empty()) {
+            auto [cost, box, tele] = pq.top(); pq.pop();
+            if (vis[box][tele]) continue;
+            vis[box][tele] = 1;
+
+            if (box == 0) return cost;
+
+            int x = box / m, y = box % m;
+
+            if (x > 0 && !vis[box - m][tele])
+                pq.push({cost + grid[x][y], box - m, tele});
+
+            if (y > 0 && !vis[box - 1][tele])
+                pq.push({cost + grid[x][y], box - 1, tele});
+
+            if (tele > 0) {
+                int currVal = grid[x][y];
+                int pos = lower_bound(
+                    cells.begin(), cells.end(),
+                    make_pair(currVal, -1)
+                ) - cells.begin();
+
+                auto &p = nextPos[tele - 1];
+                for (int i = findp(p, pos); i < N; i = findp(p, i)) {
+                    p[i] = findp(p, i + 1);
+                    int nxt = cells[i].second;
+                    if (!vis[nxt][tele - 1])
+                        pq.push({cost, nxt, tele - 1});
+                }
+            }
+        }
+        return -1;
+    }
+};

d36ec5da5ccc3bc2610a813d6b30d89585552e9e
Ph-Tejas <sharmatejas2005@gmail.com>
2026-02-04 20:59:02 +0530

    Time: 1 ms (40.54%) | Memory: 10 MB (17.44%) - LeetSync

--- 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.cpp
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     vector<vector<int>>dp;
     int findIntegers(int n) {
-        dp.resize(32,vector<int>(2));
-        dp[0][0]=1;
-        dp[0][1]=1;
+        dp.resize(32,vector<int>(2,1));
+        
         dp[1][0]=2;
-        dp[1][1]=1;
         for(int i=2;i<32;i++){
             dp[i][0]=dp[i-1][1]+dp[i-1][0];
             dp[i][1]=dp[i-1][0];

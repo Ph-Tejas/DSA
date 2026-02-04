3b6e9afa5fb876e7d65bb316a501855c46082f51
Ph-Tejas <sharmatejas2005@gmail.com>
2026-02-04 21:19:25 +0530

    Time: 2 ms (37.26%) | Memory: 8.2 MB (92.55%) - LeetSync

--- 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.cpp
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
+ int dp[32][2][2];
+
+    int dfs(string &s, int i, int tight, int prev1) {
+        if (i == s.size())
+            return 1;
+
+        if (dp[i][tight][prev1] != -1)
+            return dp[i][tight][prev1];
+
+        int limit = tight ? s[i] - '0' : 1;
+        int res = 0;
+
+        for (int bit = 0; bit <= limit; bit++) {
+            if (prev1 && bit) continue;  // no consecutive 1s
+            res += dfs(s, i + 1, tight && (bit == limit), bit);
+        }
+
+        return dp[i][tight][prev1] = res;
+    }
     int findIntegers(int n) {
-        dp.resize(32,vector<int>(2,1));
-        
-        int ct=0;
-        if(n==1)return 2;
-        if(n==0) return 1;
-        if(n==3)return 3;
-        if(n==2)return 3;
-        if(n==4)return 4;
-        vector<int>pos;
-        while(n>0){
-            if(n&1){
-                pos.push_back(ct);
-            }
-            ct++;
-            n/=2;
-        }
-        dp[1][0]=2;
-        for(int i=2;i<=pos[pos.size()-1];i++){
-            dp[i][0]=dp[i-1][1]+dp[i-1][0];
-            dp[i][1]=dp[i-1][0];
-        }
-        int ans=0;
-        for(int i=pos.size()-1;i>-1;i--){
+         string s = bitset<32>(n).to_string();
+        s = s.substr(s.find('1'));
 
-            int curr=pos[i];
-            ans+=dp[curr][0];
-            if(i!=pos.size()-1){
-                if(pos[i+1]-curr==1)break;
-            }
-            if(i==0)ans++;
-
-        }
-        return ans;
-
+        memset(dp, -1, sizeof(dp));
+        return dfs(s, 0, 1, 0);
     }
 };

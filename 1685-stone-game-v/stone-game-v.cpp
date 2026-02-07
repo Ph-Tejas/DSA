254e49a48c863562b7bbefeae00a588bc7ca32b1
Ph-Tejas <sharmatejas2005@gmail.com>
2026-02-08 00:02:30 +0530

    Time: 273 ms (88.13%) | Memory: 14 MB (100.00%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    int n;
-    vector<int>pf;
-    vector<vector<int>>dp;
-    int rec(int i, int j,vector<int>& stoneValue){
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(i==j)return dp[i][j]=0;
+int dp[501][501];
+int  solve(int i,int j,vector<int>& nums,int sum){
+    if(i==j)
+    return 0;
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+    int temp=0;
+    int ans=0;
 
-        int maxi=INT_MIN;
-
-        for(int it=i;it<j;it++){
-            int sum=0;
-            int a=pf[it+1]-pf[i];
-            int b=pf[j+1]-pf[i]-a;
-            if(a==b){
-                sum+=a;
-                sum+=max(rec(i,it,stoneValue),rec(it+1,j,stoneValue));
-
-            }
-            else if(a<b){
-                sum+=a;
-                sum+=rec(i,it,stoneValue);
-            }
-            else{
-                sum+=b;
-                sum+=rec(it+1,j,stoneValue);
-            }
-            maxi=max(maxi,sum);
-
+    for(int k=i;k<j;k++){
+        temp+=nums[k];
+        int result=0;
+        if(temp*2<sum){
+            result=temp+solve(i,k,nums,temp);
         }
-        return dp[i][j]=maxi;
+        else if(temp*2>sum){
+            result=sum-temp+solve(k+1,j,nums,sum-temp);
+        }
+        else {
+            result=temp+max(solve(i,k,nums,temp),solve(k+1,j,nums,sum-temp));
+        }
+        ans=max(ans,result);
+
+
     }
+    return dp[i][j]=ans;
+
+
+}
+
     int stoneGameV(vector<int>& stoneValue) {
-        n=stoneValue.size();
-        dp.resize(n,vector<int>(n,-1));
-        pf.resize(n+1);
-        for(int i=0;i<n;i++){
-            pf[i+1]=pf[i]+stoneValue[i];
-        }
-        return rec(0,n-1,stoneValue);
+        memset(dp,-1,sizeof(dp));
+        int n=stoneValue.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        sum+=stoneValue[i];
+        return solve(0,n-1,stoneValue,sum);
+        
     }
 };

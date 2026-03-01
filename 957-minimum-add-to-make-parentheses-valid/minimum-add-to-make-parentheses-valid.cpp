cecfd7a0b42e111bbbc5903e4b57ea3480887490
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-02 03:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (32.47%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n=s.size();
+        stack<char>st;
+        int ct=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')st.push(s[i]);
+            else{
+                if(!st.empty()){
+                    st.pop();
+                    
+                }
+                else ct++;
+            }
+        }
+        return ct+st.size();
+    }
+};

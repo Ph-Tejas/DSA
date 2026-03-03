4b9e9bd72223e1581417009269708846ff13cc30
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-03 08:17:52 +0530

    Time: 2 ms (82.91%) | Memory: 10.4 MB (30.28%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,21 +1,74 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int repeat = b.size() / a.size();
-        int count = 1;
-
-        while (count <= repeat + 2) {
-            string temp = "";
-            for (int i = 0; i < count; i++) {
-                temp += a;
+    void formLPS(string b,int m,vector<int>&lps){
+        
+        int len=0;
+        int i=1;
+        while(i<m){
+            if(b[i]==b[len]){
+                len++;
+                lps[i]=len;
+                i++;
+                
             }
-
-            if (temp.find(b) != string::npos) {
-                return count;
-            } else {
-                count++;
+            else{
+                if(len==0){
+                    lps[i]=0;
+                    i++;
+                }
+                else{
+                    len=lps[len-1];
+                }
             }
         }
+    }
+    int repeatedStringMatch(string a, string b) {
+        int n=a.size();
+        int m=b.size();
+        vector<int>cases={(m/n),(m/n)+1,(m/n)+2,(m/n)+3};
+        string temp="";
+        for(int i=0;i<cases[0];i++){
+            temp+=a;
+        }
+        vector<int>lps(m);
+        formLPS(b,m,lps);
+        
+        for(int ct=0;ct<4;ct++){
+
+            int i=0;
+            int j=0;
+            while(i<temp.size()){
+                if(b[j]==temp[i]){
+                    i++;
+                    j++;
+                }
+                if(j==m){
+                    return cases[ct];
+                }
+                
+                else if(i<temp.size()&&b[j]!=temp[i]){
+                    if(j==0){
+                        i++;
+                    }
+                    else{
+                        j=lps[j-1];
+
+                    }
+                }
+            }
+
+            
+            
+
+
+            temp+=a;
+        }
         return -1;
+
+
+
+
+
+
     }
 };

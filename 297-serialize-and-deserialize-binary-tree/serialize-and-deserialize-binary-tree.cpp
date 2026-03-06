4e97f55d82f749a84f24255fd40e877a2ef56d27
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-06 11:33:12 +0530

    Time: 34 ms (22.78%) | Memory: 32.4 MB (53.80%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -1,101 +1,44 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Codec {
 public:
-
-    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        string fin="";
-        queue<TreeNode*>q;
+        if (!root) return "null";
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
-            int value;
-            TreeNode* l;
-            TreeNode* r;
-            if(q.front()==NULL){
-                fin+="null#";
-                q.pop();
-                continue;
+        string res;
+        while (!q.empty()) {
+            TreeNode* node = q.front(); q.pop();
+            if (node) {
+                res += to_string(node->val) + ",";
+                q.push(node->left);
+                q.push(node->right);
+            } else {
+                res += "null,";
             }
-            value=q.front()->val;
-            l=q.front()->left;
-            r=q.front()->right;
-            fin+=to_string(value);
-            fin+="#";
-            q.pop();
-            q.push(l);
-            q.push(r);
-
-
-
         }
-        return fin;
-
+        return res;
     }
 
-    // Decodes your encoded data to tree.
-    void rec(int &it,vector<int>&fin,vector<TreeNode*>toPursue){
-        if(toPursue.size()==0)return;
-        vector<TreeNode*>willPursue;
-        for(int i=0;i<toPursue.size();i++){
-            if(toPursue[i]==NULL)continue;
-            if(fin[it]==2000){
-                toPursue[i]->left=NULL;
-            }
-            else toPursue[i]->left=new TreeNode(fin[it]);
-            willPursue.push_back(toPursue[i]->left);
-            it++;
-            if(fin[it]==2000){
-                toPursue[i]->right=NULL;
-
-            }
-            else{
-                toPursue[i]->right=new TreeNode(fin[it]);
-            }
-            willPursue.push_back(toPursue[i]->right);
-            it++;
-        }
-        rec(it,fin,willPursue);
-
-    }
     TreeNode* deserialize(string data) {
-        
-        vector<int>fin;
-        string temp;
-        for(int i=0;i<data.size();i++){
-            if(data[i]=='#'){
-                if(temp[0]=='n'){
-                    fin.push_back(2000);
-                }
-                else fin.push_back(stoi(temp));
-                temp.clear();
-                continue;
+        if (data == "null") return nullptr;
+        stringstream ss(data);
+        string token;
+        getline(ss, token, ',');
+        TreeNode* root = new TreeNode(stoi(token));
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front(); q.pop();
+            if (!getline(ss, token, ',')) break;
+            if (token != "null") {
+                node->left = new TreeNode(stoi(token));
+                q.push(node->left);
             }
-            temp.push_back(data[i]);
-
+            if (!getline(ss, token, ',')) break;
+            if (token != "null") {
+                node->right = new TreeNode(stoi(token));
+                q.push(node->right);
+            }
         }
-        if(fin.size()==1){
-            return NULL;
-        }
-        int it=1;
-        TreeNode* head=new TreeNode(fin[0]);
-        
-        rec(it,fin,{head});
-        return head;
-        
-
-
-
+        return root;
     }
 };
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser, deser;
-// TreeNode* ans = deser.deserialize(ser.serialize(root));

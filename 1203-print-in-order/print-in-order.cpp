1f65e5408b075d61d0afd89df4ea08448f45aaf3
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-07 15:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (47.64%) - LeetSync

--- 1203-print-in-order/print-in-order.cpp
+++ 1203-print-in-order/print-in-order.cpp
@@ -1,6 +1,6 @@
 class Foo {
 public:
-    stack<int>st;
+    int st=0;
     mutex m;
     condition_variable not_empty;
     condition_variable not_first;
@@ -13,33 +13,32 @@ public:
         unique_lock<mutex> lock(m);
         // printFirst() outputs "first". Do not change or remove this line.
         not_empty.wait(lock,[&](){
-            return (st.empty());
+            return (st==0);
         });
+        st=1;
         printFirst();
-        st.push(1);
         not_first.notify_one();
     }
 
     void second(function<void()> printSecond) {
         unique_lock<mutex> lock(m);
         not_first.wait(lock,[&](){
-            return ((!st.empty())&&st.top()==1);
+            return (st==1);
         });
+        st=2;
         // printSecond() outputs "second". Do not change or remove this line.
         printSecond();
-        st.push(2);
         not_second.notify_one();
     }
 
     void third(function<void()> printThird) {
         unique_lock<mutex> lock(m);
         not_second.wait(lock,[&](){
-            return ((!st.empty())&&st.top()==2);
+            return (st==2);
         });
+        st=0;
         // printThird() outputs "third". Do not change or remove this line.
         printThird();
-        st.pop();
-        st.pop();
         not_empty.notify_one();
     }
 };

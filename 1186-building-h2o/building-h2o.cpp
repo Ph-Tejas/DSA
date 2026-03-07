8b3118a4daa41d7b4f0f359f66dd15f2941448ef
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-07 17:16:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (20.22%) - LeetSync

---
+++ 1186-building-h2o/building-h2o.cpp
@@ -0,0 +1,38 @@
+class H2O {
+public:
+    int o=0;
+    int h=0;
+    mutex m;
+    condition_variable cv; 
+    H2O() {
+        
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        unique_lock<mutex>lock(m);
+        cv.wait(lock,[&](){
+            return (o==1);
+        });
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen();
+        h++;
+        if(h==2){
+            h=0;
+            o=0;
+        }
+        cv.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        unique_lock<mutex>lock(m);
+        cv.wait(lock,[&](){
+            return (o==0);
+        });
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+        o++;
+
+        cv.notify_all();
+
+    }
+};

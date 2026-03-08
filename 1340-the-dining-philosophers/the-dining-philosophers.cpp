802a39b5085cf47ac8f9911aa3ac9bb3052971fc
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-08 16:12:42 +0530

    Time: 36 ms (40.11%) | Memory: 17.5 MB (97.71%) - LeetSync

--- 1340-the-dining-philosophers/the-dining-philosophers.cpp
+++ 1340-the-dining-philosophers/the-dining-philosophers.cpp
@@ -1,15 +1,10 @@
 class DiningPhilosophers {
 public:
     mutex m;
-    condition_variable cv0;
-    condition_variable cv1;
-    condition_variable cv2;
-    condition_variable cv3;
-    condition_variable cv4;
-    bool f0=false,f1=false,f2=false,f3=false,f4=false;
-    DiningPhilosophers() {
-        
-    }
+    condition_variable cv[5];
+    bool f[5] = {false,false,false,false,false};
+
+    DiningPhilosophers() {}
 
     void wantsToEat(int philosopher,
                     function<void()> pickLeftFork,
@@ -18,110 +13,31 @@ public:
                     function<void()> putLeftFork,
                     function<void()> putRightFork) {
 
-                        unique_lock<mutex>lock(m);
-                        
-                        if(philosopher==0){
-                            cv0.wait(lock,[&](){
-                                return f0==false;
-                            });
-                            f0=true;
-                            pickLeftFork();
-                            cv4.wait(lock,[&](){
-                                return f4==false;
-                            });
-                            f4=true;
-                            pickRightFork();
-                            eat();
-                            f0=false;
-                            putLeftFork();
+        unique_lock<mutex> lock(m);
 
-                            cv0.notify_all();
-                            f4=false;
-                            putRightFork();
-                            cv4.notify_all();
-                        }
-                        if(philosopher==1){
-                            cv1.wait(lock,[&](){
-                                return f1==false;
-                            });
-                            f1=true;
-                            pickLeftFork();
-                            cv0.wait(lock,[&](){
-                                return f0==false;
-                            });
-                            f0=true;
-                            pickRightFork();
-                            eat();
-                            f1=false;
-                            putLeftFork();
+        int left = philosopher;
+        int right = (philosopher + 4) % 5;
 
-                            cv1.notify_all();
-                            f0=false;
-                            putRightFork();
-                            cv0.notify_all();
-                        }
-                        if(philosopher==2){
-                            cv2.wait(lock,[&](){
-                                return f2==false;
-                            });
-                            f2=true;
-                            pickLeftFork();
-                            cv1.wait(lock,[&](){
-                                return f1==false;
-                            });
-                            f1=true;
-                            pickRightFork();
-                            eat();
-                            f2=false;
-                            putLeftFork();
+        cv[left].wait(lock,[&](){
+            return f[left] == false;
+        });
+        f[left] = true;
+        pickLeftFork();
 
-                            cv2.notify_all();
-                            f1=false;
-                            putRightFork();
-                            cv1.notify_all();
-                        }
-                        if(philosopher==3){
-                            cv3.wait(lock,[&](){
-                                return f3==false;
-                            });
-                            f3=true;
-                            pickLeftFork();
-                            cv2.wait(lock,[&](){
-                                return f2==false;
-                            });
-                            f2=true;
-                            pickRightFork();
-                            eat();
-                            f3=false;
-                            putLeftFork();
+        cv[right].wait(lock,[&](){
+            return f[right] == false;
+        });
+        f[right] = true;
+        pickRightFork();
 
-                            cv3.notify_all();
-                            f2=false;
-                            putRightFork();
-                            cv2.notify_all();
+        eat();
 
-                        }
-                        if(philosopher==4){
-                            cv4.wait(lock,[&](){
-                                return f4==false;
-                            });
-                            f4=true;
-                            pickLeftFork();
-                            cv3.wait(lock,[&](){
-                                return f3==false;
-                            });
-                            f3=true;
-                            pickRightFork();
-                            eat();
-                            f4=false;
-                            putLeftFork();
+        f[left] = false;
+        putLeftFork();
+        cv[left].notify_one();
 
-                            cv4.notify_all();
-                            f3=false;
-                            putRightFork();
-                            cv3.notify_all();
-                        }
-                        
-		
+        f[right] = false;
+        putRightFork();
+        cv[right].notify_one();
     }
 };

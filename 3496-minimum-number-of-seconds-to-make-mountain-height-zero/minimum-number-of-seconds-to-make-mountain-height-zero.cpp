45ca78373d6141813104238cf5e5c73b9600d062
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-13 17:46:23 +0530

    Time: 16 ms (72.90%) | Memory: 29.1 MB (30.32%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -11,19 +11,31 @@ public:
             for(int i=0;i<sz;i++){
                 
                 long long curr=workerTime[i];
-                long long lo=0;
-                long long hi=1e6;
-                long long mid=lo+(hi-lo)/2;
-                while(lo<=hi){
-                    if(curr*((mid*(mid+1))/2)<=mid1){
-                        lo=mid+1;
-
-                    }
-                    else hi=mid-1;
-                    mid=lo+(hi-lo)/2;
-
+                long long val=sqrt((mid1/curr)*2)+1;
+                if(curr*((val*(val+1))/2)<=mid1){
+                    mH-=val;
                 }
-                mH-=hi;
+                else if(curr*((val*(val-1))/2)<=mid1){
+                    mH-=val-1;
+                }
+                else{
+                    mH-=val-2;
+                }
+                
+
+                // long long lo=0;
+                // long long hi=1e6;
+                // long long mid=lo+(hi-lo)/2;
+                // while(lo<=hi){
+                //     if(curr*((mid*(mid+1))/2)<=mid1){
+                //         lo=mid+1;
+
+                //     }
+                //     else hi=mid-1;
+                //     mid=lo+(hi-lo)/2;
+
+                // }
+                // mH-=hi;
                 if(mH<=0)break;
             }
             if(mH<=0){

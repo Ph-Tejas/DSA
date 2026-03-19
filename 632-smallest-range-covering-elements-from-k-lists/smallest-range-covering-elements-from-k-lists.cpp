5ebb81167e7e130ac782de5da804747bc564c235
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-19 19:24:03 +0530

    Time: 274 ms (20.52%) | Memory: 58.8 MB (24.40%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int n=nums.size();
+        vector<int>ptrs(n,0);
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(nums[i].size()==1)pq.push({nums[i][0],1000000000,i});
+            else pq.push({nums[i][0],nums[i][1],i});
+            maxi=max(maxi,nums[i][0]);
+        }
+        int ans=INT_MAX;
+        int l=-1;
+        int r=-1;
+        while(!pq.empty()){
+            int a=pq.top()[0];
+            int b=pq.top()[1];
+            int c=pq.top()[2];
+            pq.pop();
+            if(a==1000000000)break;
+            if(ans>maxi-a){
+                ans=maxi-a;
+                l=a;
+                r=maxi;
+            }
+        
+            a=b;
+
+            if(ptrs[c]+1<nums[c].size())b=nums[c][ptrs[c]+1];
+            else b=1e9;
+            if(a>maxi)maxi=a;
+            ptrs[c]++;
+            pq.push({a,b,c});
+            
+
+        }
+        return {l,r};
+
+
+        
+
+    }
+};

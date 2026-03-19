6cc1a1fcf3b48434afbeebb533b4b08ac89d196c
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-19 16:20:50 +0530

    Time: 31 ms (38.07%) | Memory: 39.8 MB (8.03%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -7,17 +7,22 @@ public:
             else return 0;
         }
         if(k==0)return 1;
-        vector<vector<long long>>dp(n+1,vector<long long>(1001));
+        vector<vector<long long>>dp(n+1,vector<long long>(k+1));
         dp[1][0]=1;
         dp[2][0]=2;
         dp[2][1]=1;
         for(int i=3;i<=n;i++){
-            for(int j=1000;j>-1;j--){
-                if(j==1000)dp[i][j]=dp[i-1][1000-i+1];
-                else if(j==0)dp[i][j]=1+dp[i][1];
+            for(int j=k;j>-1;j--){
+                if(j==k){
+                    if(i-1>j){
+                        dp[i][j]=(dp[i-1][0]);
+                    }
+                    else dp[i][j]=dp[i-1][k-i+1];
+                }
                 else if(i-1>j){
                     dp[i][j]=(dp[i-1][0]-dp[i-1][j+1]+mod)+dp[i][j+1];
                 }
+                else if(j==0)dp[i][j]=1+dp[i][1];
                 else{
                     dp[i][j]=(dp[i-1][j-i+1]-dp[i-1][j+1]+mod)+dp[i][j+1];
                 }
@@ -25,7 +30,7 @@ public:
                 
             }
         }
-        return (k==1000)?(dp[n][k]):(dp[n][k]-dp[n][k+1]+mod)%mod;
+        return (dp[n][k]);
 
 
         

45b33b0800b93023aed9cfd5c7a1635444ec7a2d
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-23 05:27:55 +0530

    Time: 599 ms (53.28%) | Memory: 78.8 MB (56.78%) - LeetSync

--- 1290-make-array-strictly-increasing/make-array-strictly-increasing.cpp
+++ 1290-make-array-strictly-increasing/make-array-strictly-increasing.cpp
@@ -1,13 +1,9 @@
-struct hash_pair {
-    size_t operator()(const pair<int,int>& p) const {
-        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-    }
-};
+
 class Solution {
 public:
 
 
-    unordered_map<pair<int,int>, int, hash_pair> mp;
+    map<pair<int,int>, int> mp;
     int fun(int ind,int prev,vector<int>& arr1, vector<int>& arr2){
         if(ind==arr1.size())return 0;
         if(mp.find({ind,prev})!=mp.end()){
@@ -15,11 +11,12 @@ public:
         }
         int changing;
         int notChanging;
-        if(upper_bound(arr2.begin(),arr2.end(),prev)==arr2.end()){
+        auto it=upper_bound(arr2.begin(),arr2.end(),prev);
+        if(it==arr2.end()){
             changing=1e9;
         }
         else {
-            int val=(*upper_bound(arr2.begin(),arr2.end(),prev));
+            int val=(*it);
             changing=1+fun(ind+1,val,arr1,arr2);
         }
         if(arr1[ind]<=prev){

bc901ecc0cc7bc31632613acbbe8c2f146a3e318
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-23 05:21:40 +0530

    Time: 623 ms (50.47%) | Memory: 78.8 MB (54.91%) - LeetSync

---
+++ 1290-make-array-strictly-increasing/make-array-strictly-increasing.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    map<pair<int,int>,int>mp;
+    int fun(int ind,int prev,vector<int>& arr1, vector<int>& arr2){
+        if(ind==arr1.size())return 0;
+        if(mp.find({ind,prev})!=mp.end()){
+            return mp[{ind,prev}];
+        }
+        int changing;
+        int notChanging;
+        if(upper_bound(arr2.begin(),arr2.end(),prev)==arr2.end()){
+            changing=1e9;
+        }
+        else {
+            int val=(*upper_bound(arr2.begin(),arr2.end(),prev));
+            changing=1+fun(ind+1,val,arr1,arr2);
+        }
+        if(arr1[ind]<=prev){
+            notChanging=1e9;
+            
+        }
+        else{
+            notChanging=fun(ind+1,arr1[ind],arr1,arr2);
+            
+        }
+        return mp[{ind,prev}]=min(changing,notChanging);
+    }
+
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        int n=arr1.size();
+        int m=arr2.size();
+        sort(arr2.begin(),arr2.end());
+        int ans=fun(0,-1,arr1,arr2);
+        if(ans>=1e9)return -1;
+        return ans;
+
+
+
+
+
+
+
+    }
+};

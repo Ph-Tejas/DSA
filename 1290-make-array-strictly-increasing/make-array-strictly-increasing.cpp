6abee9ac0b7a4178500ba6b3a1f199fe373ce0aa
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-23 05:29:16 +0530

    Time: 456 ms (56.78%) | Memory: 82.9 MB (14.49%) - LeetSync

--- 1290-make-array-strictly-increasing/make-array-strictly-increasing.cpp
+++ 1290-make-array-strictly-increasing/make-array-strictly-increasing.cpp
@@ -1,49 +1,40 @@
-
 class Solution {
 public:
 
+    struct hash_pair {
+        size_t operator()(const pair<int,int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
 
-    map<pair<int,int>, int> mp;
-    int fun(int ind,int prev,vector<int>& arr1, vector<int>& arr2){
-        if(ind==arr1.size())return 0;
-        if(mp.find({ind,prev})!=mp.end()){
-            return mp[{ind,prev}];
+    unordered_map<pair<int,int>, int, hash_pair> mp;
+    const int INF = INT_MAX / 2;
+
+    int fun(int ind, int prev, vector<int>& arr1, vector<int>& arr2) {
+        if (ind == arr1.size()) return 0;
+
+        if (mp.count({ind, prev})) return mp[{ind, prev}];
+
+        int changing = INF, notChanging = INF;
+
+        auto it = upper_bound(arr2.begin(), arr2.end(), prev);
+
+        if (it != arr2.end()) {
+            changing = 1 + fun(ind + 1, *it, arr1, arr2);
         }
-        int changing;
-        int notChanging;
-        auto it=upper_bound(arr2.begin(),arr2.end(),prev);
-        if(it==arr2.end()){
-            changing=1e9;
+
+        if (arr1[ind] > prev) {
+            notChanging = fun(ind + 1, arr1[ind], arr1, arr2);
         }
-        else {
-            int val=(*it);
-            changing=1+fun(ind+1,val,arr1,arr2);
-        }
-        if(arr1[ind]<=prev){
-            notChanging=1e9;
-            
-        }
-        else{
-            notChanging=fun(ind+1,arr1[ind],arr1,arr2);
-            
-        }
-        return mp[{ind,prev}]=min(changing,notChanging);
+
+        return mp[{ind, prev}] = min(changing, notChanging);
     }
 
     int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-        int n=arr1.size();
-        int m=arr2.size();
-        sort(arr2.begin(),arr2.end());
+        sort(arr2.begin(), arr2.end());
         arr2.erase(unique(arr2.begin(), arr2.end()), arr2.end());
-        int ans=fun(0,-1,arr1,arr2);
-        if(ans>=1e9)return -1;
-        return ans;
 
-
-
-
-
-
-
+        int ans = fun(0, -1, arr1, arr2);
+        return (ans >= INF) ? -1 : ans;
     }
 };

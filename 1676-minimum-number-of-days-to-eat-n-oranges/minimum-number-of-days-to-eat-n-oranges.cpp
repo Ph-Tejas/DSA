2d97a60e47ccfdd39ebe54d230b61c8b9dca0d59
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-26 05:19:20 +0530

    Time: 7 ms (68.06%) | Memory: 11.5 MB (58.71%) - LeetSync

--- 1676-minimum-number-of-days-to-eat-n-oranges/minimum-number-of-days-to-eat-n-oranges.cpp
+++ 1676-minimum-number-of-days-to-eat-n-oranges/minimum-number-of-days-to-eat-n-oranges.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    map<int,int>mp;
     int fun(int n){
+
         if(n==1)return 1;
         if(n==2)return 2;
         if(n==3)return 2;
+        if(mp.find(n)!=mp.end()){
+            return mp[n];
+        }
         
         int mini=INT_MAX;
-        // if(n%3==0){
-        //     mini=min(mini,fun(n/3)+1);
-        // }
-        // if(n%2==0){
-        //     mini=min(mini,fun(n/2)+1);
-        // }
         int ct=n%3;
         int ct2=n%2;
         mini=min(mini,fun((n-ct)/3)+ct+1);
@@ -25,7 +24,7 @@ public:
         
 
 
-        return mini;
+        return mp[n]=mini;
     }
     int minDays(int n) {
 

5657124c23fed94a7232574f9a305cfc5f0cd1cb
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-27 06:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (49.69%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,61 +1,15 @@
 class Solution {
 public:
-    int gcd(int a,int b){
-        if(b>a){
-            int temp=a;
-            a=b;
-            b=temp;
-        }
-        if(a%b==0)return b;
-        return gcd(b,a%b);
-
-
-
-    }
-    
-    bool areSimilar(vector<vector<int>>& mat, int K) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<int>lcm;
-
-        vector<int>vis(n); 
-        for(int i=1;i<=m;i++){
-            if (m%i!=0)continue;
-            for(int j=0;j<n;j++){
-                if(vis[j]==1)continue;
-                vector<int>temp;
-                for(int k=0;k<i;k++){
-                    temp.push_back(mat[j][k]);
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][(j+k)%m] != mat[i][j]){
+                    return false;
                 }
-                int it=0;
-                for(int k=0;k<m;k++){
-                    if(mat[j][k]!=temp[it]){
-                        break;
-
-                    }
-                    if(k==m-1){
-                        lcm.push_back(i);
-                        vis[j]=1;
-                        
-                    }
-
-                    it++;
-                    if(it==i)it=0;
-                    
-
-                }
-
             }
         }
-        int sz=lcm.size();
-        int ans=1;
-        
-        for(int i=0;i<sz;i++){
-            ans=(ans*lcm[i])/gcd(ans,lcm[i]);
-        }
-        
-        if(K%ans==0)return true;
-        return false;
-
+        return true;
     }
 };

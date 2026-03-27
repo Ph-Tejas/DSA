cccf5c4208b1c445c2e1d988750e478815e345c4
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-28 02:37:41 +0530

    Time: 63 ms (84.73%) | Memory: 75.9 MB (48.09%) - LeetSync

--- 1947-number-of-different-subsequences-gcds/number-of-different-subsequences-gcds.cpp
+++ 1947-number-of-different-subsequences-gcds/number-of-different-subsequences-gcds.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    int gcd(int a,int b){
-        if(b>a){
-            int temp=a;
-            a=b;
-            b=temp;
-        }
-        if(a%b==0)return b;
-        return gcd(b,a%b);
-    }
     int countDifferentSubsequenceGCDs(vector<int>& nums) {
-        int n=nums.size();
-        int maxi=*max_element(nums.begin(),nums.end());
-        vector<int>v(maxi+1);
-        int ct=0;
-        vector<bool>present(maxi+1);
-        for(int i=0;i<n;i++){
-            int curr=nums[i];
-            present[curr]=true;
-        }
+        int maxi = *max_element(nums.begin(), nums.end());
 
+        vector<bool> present(maxi + 1, false);
+        for (int x : nums) present[x] = true;
 
-        for(int i=1;i<=maxi;i++){
-            int letsGCD=-1;
-            for(int j=i;j<=maxi;j+=i){
-                if(letsGCD==-1&&present[j]){
-                    letsGCD=j;
-                }
-                else if(present[j]){
-                    letsGCD=gcd(letsGCD,j);
-                    if(letsGCD==i)break;
+        int ct = 0;
+
+        for (int i = 1; i <= maxi; i++) {
+            int g = 0;
+
+            for (int j = i; j <= maxi; j += i) {
+                if (!present[j]) continue;
+
+                g = __gcd(g, j);
+
+                if (g == i) {
+                    ct++;
+                    break;
                 }
             }
-            if(letsGCD==i){
-                ct++;
-            }
         }
+
         return ct;
-        
-
-
     }
 };

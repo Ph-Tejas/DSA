fee385c19396a598c143e4ed57b74897bfc65433
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-27 23:57:16 +0530

    Time: 1290 ms (7.89%) | Memory: 296.4 MB (16.60%) - LeetSync

--- 1772-create-sorted-array-through-instructions/create-sorted-array-through-instructions.cpp
+++ 1772-create-sorted-array-through-instructions/create-sorted-array-through-instructions.cpp
@@ -1,40 +1,32 @@
-
 #include <ext/pb_ds/assoc_container.hpp> 
 #include <ext/pb_ds/tree_policy.hpp> 
 using namespace __gnu_pbds;
-// typedef tree<int, null_type, greater_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set1;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
 class Solution {
 public:
     int createSortedArray(vector<int>& instructions) {
-        int n=instructions.size();
-        int mod=1e9+7;
+        int n = instructions.size();
+        int mod = 1e9 + 7;
+
         ordered_set st;
-        long long ans=0;
-        map<int,int>mp;
-        for(int i=0;i<n;i++){
-            int curr=instructions[i];
-            int ct1=0,ct2=0;
-            if(st.empty()){
-                st.insert(curr);
-                mp[curr]++;
-                continue;
-            }
-            ct1=st.order_of_key(curr);
+        long long ans = 0;
 
+        for(int i = 0; i < n; i++) {
+            int curr = instructions[i];
 
-            
-            ct2=st.size()-ct1-mp[curr];
-            
-            ans+=min(ct1,ct2);
-            ans%=mod;
-            st.insert(curr);
-            mp[curr]++;
+            // count strictly smaller
+            int ct1 = st.order_of_key({curr, 0});
 
+            // count strictly greater
+            int ct2 = st.size() - st.order_of_key({curr, INT_MAX});
 
+            ans = (ans + min(ct1, ct2)) % mod;
 
-
+            st.insert({curr, i}); // unique index
         }
+
         return ans;
     }
-};
+}; 

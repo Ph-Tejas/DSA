ababb1497c1558c637e9e8dbd6d7c4343e4ecebd
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-29 06:50:11 +0530

    Time: 1483 ms (9.09%) | Memory: 405 MB (9.09%) - LeetSync

--- 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.cpp
+++ 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int mod=1e9+7;
-    vector<vector<long long>> collection;
-    int binPow(long long n,long long p){
-        long long mul=1;
-        for(int i=0;i<=61;i++){
-            if((p>>i)&1){
-                mul*=collection[n][i];
-                mul%=mod;
-            }
-        }
-        return mul;
+    long long binPow(long long a, long long p){
+    vector<long long> temp(62,1);
+    temp[0] = a % mod;
 
+    for(int i=1;i<=61;i++){
+        temp[i] = (temp[i-1] * temp[i-1]) % mod;
     }
+
+    long long mul = 1;
+    for(int i=0;i<=61;i++){
+        if((p>>i)&1){
+            mul *= temp[i];
+            mul %= mod;
+        }
+    }
+    return mul;
+}
     int multInv(int n){
         return binPow(n,mod-2)%mod;
     }
 
     int countVisiblePeople(int n, int pos, int k) {
-        collection.resize(n+1,vector<long long>(62,1));
-        for(int i=1;i<=n;i++){
-            collection[i][0] = i;
-            for(int j=1;j<=61;j++){
-                collection[i][j] = (collection[i][j-1] * collection[i][j-1]) % mod;
-            }
-        }
         long long X=pos;
         long long N_X=n-X-1;
         vector<long long>x(k+1,1);

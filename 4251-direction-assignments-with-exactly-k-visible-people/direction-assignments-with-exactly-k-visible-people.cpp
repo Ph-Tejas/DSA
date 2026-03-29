bd308392b18234e5bdc87db838d4241feb03033b
Ph-Tejas <sharmatejas2005@gmail.com>
2026-03-29 06:57:41 +0530

    Time: 3 ms (100.00%) | Memory: 8.7 MB (90.91%) - LeetSync

--- 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.cpp
+++ 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.cpp
@@ -1,59 +1,36 @@
+typedef long long ll;
+
+const ll MOD=1e9+7;
+
 class Solution {
 public:
-    int mod=1e9+7;
-long long binPow(long long a, long long p){
-    long long res = 1;
-    while(p){
-        if(p & 1) res = (res * a) % mod;
-        a = (a * a) % mod;
-        p >>= 1;
+    ll power(ll base, ll exp) {
+        ll res=1;
+        base%=MOD;
+        while (exp>0) {
+            if (exp%2==1) res=(res*base)%MOD;
+            base=(base*base)%MOD;
+            exp/=2;
+        }
+        return res;
     }
-    return res;
-}
-    int multInv(int n){
-        return binPow(n,mod-2)%mod;
+    ll modInverse(ll n) {
+        return power(n,MOD-2);
     }
+    ll nCr(int n, int k) {
+        if (k<0 || k>n) return 0;
+        if (k==0 || k==n) return 1;
+        if (k>n/2) k=n-k;
 
+        ll num=1,den=1;
+        for (int i=0;i<k;i++) {
+            num=(num*(n-i))%MOD;
+            den=(den*(i+1))%MOD;
+        }
+        return (num*modInverse(den))%MOD;
+    }
     int countVisiblePeople(int n, int pos, int k) {
-        long long X=pos;
-        long long N_X=n-X-1;
-        vector<long long>x(k+1,1);
-        vector<long long>n_x(k+1,1);
-        for(int i=1;i<=k;i++){
-            if(i>X)break;
-            x[i]=x[i-1]*(X-i+1);
-            x[i]%=mod;
-            x[i]*=multInv(i);
-            x[i]%=mod;
-
-
-        }
-        for(int i=1;i<=k;i++){
-            if(i>N_X)break;
-            n_x[i]=n_x[i-1]*(N_X-i+1);
-            n_x[i]%=mod;
-            n_x[i]*=multInv(i);
-            n_x[i]%=mod;
-
-
-        }
-        long long ans=0;
-        if(pos==0){
-            return (n_x[k]*2)%mod;
-        }
-        if(pos==n-1){
-            return (x[k]*2)%mod;
-        }
-        else{
-            for(int i=0;i<=k;i++){
-                if(k-i>N_X)continue;
-                if(i>X)continue;
-                ans+=((x[i]*n_x[k-i])%mod);
-                ans%=mod;
-            }
-        }
-        return (ans*2)%mod;
-        
-
+        // total ways = n-1Ck
+        return (nCr(n-1,k)*2)%MOD;
     }
 };

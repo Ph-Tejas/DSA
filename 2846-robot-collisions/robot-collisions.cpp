2e33a2f2011a11deed86d9ae1e2c9147de2c1bf0
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-01 23:06:43 +0530

    Time: 60 ms (48.70%) | Memory: 208.5 MB (29.34%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,74 +1,64 @@
 class Solution {
 public:
+    struct Robot {
+        int pos, health, dir, idx;
+    };
+
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        vector<vector<int>>v;
-        int n=positions.size();
-        int charc;
-        for(int i=0;i<n;i++){
-            if(directions[i]=='L')charc=0;
-            else charc=1;
-            v.push_back({positions[i],healths[i],charc,i});
+        int n = positions.size();
+        vector<Robot> v(n);
+
+        for(int i = 0; i < n; i++) {
+            v[i] = {positions[i], healths[i], directions[i] == 'R', i};
         }
-        sort(v.begin(),v.end(),[](vector<int>&a,vector<int>&b){
-            return a[0]<b[0];
+
+        sort(v.begin(), v.end(), [](Robot &a, Robot &b) {
+            return a.pos < b.pos;
         });
-        vector<pair<int,int>>ans;
-        stack<pair<int,int>>st;
 
-        for(int i=0;i<n;i++){
-            if(v[i][2]==0){
-                if(st.empty()){
-                
-                    ans.push_back({v[i][1],v[i][3]});
+        stack<Robot> st;
+        vector<pair<int,int>> ans;
+
+        for(int i = 0; i < n; i++) {
+            if(v[i].dir == 1) {
+                st.push(v[i]);
+            } 
+            else {
+                int currHealth = v[i].health;
+
+                while(!st.empty() && currHealth > st.top().health) {
+                    currHealth--;
+                    st.pop();
                 }
-                else{
-                    while((!st.empty()) && v[i][1]>st.top().first){
-                        v[i][1]-=1;
+
+                if(st.empty()) {
+                    if(currHealth > 0)
+                        ans.push_back({currHealth, v[i].idx});
+                } 
+                else {
+                    if(currHealth == st.top().health) {
                         st.pop();
-                    }
-                    if(st.empty()){
-                        if(v[i][1]!=0){
-                            ans.push_back({v[i][1],v[i][3]});
-                            
-                        }
-                    }
-                    else{
-                        if(v[i][1]==st.top().first){
-                            st.pop();
-                            continue;
-                        }
-                        if(v[i][1]!=0){
-                            int temp=st.top().first;
-                            int ind=st.top().second;
-                            
-                            temp-=1;
-                            st.pop();
-                            if(temp!=0){
-                                st.push({temp,ind});
-                            }
-
-                        }
-
+                    } 
+                    else {
+                        Robot top = st.top(); st.pop();
+                        top.health--;
+                        if(top.health > 0) st.push(top);
                     }
                 }
             }
-            else{
-                st.push({v[i][1],v[i][3]});
-            }
-
         }
-        while(!st.empty()){
-            ans.push_back({st.top().first,st.top().second});
+
+        while(!st.empty()) {
+            ans.push_back({st.top().health, st.top().idx});
             st.pop();
         }
-        sort(ans.begin(),ans.end(),[](pair<int,int>&a,pair<int,int>&b){
-            return a.second<b.second;
-        });
-        vector<int>fin;
-        for(int i=0;i<ans.size();i++){
-            fin.push_back(ans[i].first);
-        }
-        return fin;
 
+        sort(ans.begin(), ans.end(), [](auto &a, auto &b) {
+            return a.second < b.second;
+        });
+
+        vector<int> res;
+        for(auto &p : ans) res.push_back(p.first);
+        return res;
     }
 };

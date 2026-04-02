664909cbd35795d944452826c08d069fd4a81b21
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-02 14:55:35 +0530

    Time: 503 ms (59.58%) | Memory: 276 MB (37.87%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -8,61 +8,64 @@ public:
 
 
         for(int i=0;i<n;i++){
+            int curr=coins[i][0];
             if(i==0){
                 
-                if(coins[0][0]>=0){
-                    dp[i][0][2]=coins[0][0];
-                    dp[i][0][1]=coins[0][0];
-                    dp[i][0][0]=coins[0][0];
+                if(curr>=0){
+                    dp[i][0][2]=curr;
+                    dp[i][0][1]=curr;
+                    dp[i][0][0]=curr;
                     
                 }
                 else {
-                    dp[i][0][2]=coins[0][0];
+                    dp[i][0][2]=curr;
                     dp[i][0][1]=0;
                     dp[i][0][0]=0;
                 }
             }
             else{
-                dp[i][0][2]=dp[i-1][0][2]+coins[i][0];
-                if(coins[i][0]>=0){
-                    dp[i][0][1]=dp[i-1][0][1]+coins[i][0];
-                    dp[i][0][0]=dp[i-1][0][0]+coins[i][0];
+                dp[i][0][2]=dp[i-1][0][2]+curr;
+                if(curr>=0){
+                    dp[i][0][1]=dp[i-1][0][1]+curr;
+                    dp[i][0][0]=dp[i-1][0][0]+curr;
                     
                 }
                 else{
-                    dp[i][0][1]=max(dp[i-1][0][1]+coins[i][0],dp[i-1][0][2]);
-                    dp[i][0][0]=max(dp[i-1][0][0]+coins[i][0],dp[i-1][0][1]);
+                    dp[i][0][1]=max(dp[i-1][0][1]+curr,dp[i-1][0][2]);
+                    dp[i][0][0]=max(dp[i-1][0][0]+curr,dp[i-1][0][1]);
                 }
                 
             }
         }
         for(int i=1;i<m;i++){
+            int curr=coins[0][i];
             
            
-            dp[0][i][2]=dp[0][i-1][2]+coins[0][i];
-            if(coins[0][i]>=0){
-                dp[0][i][1]=dp[0][i-1][1]+coins[0][i];
-                dp[0][i][0]=dp[0][i-1][0]+coins[0][i];
+            dp[0][i][2]=dp[0][i-1][2]+curr;
+            if(curr>=0){
+                dp[0][i][1]=dp[0][i-1][1]+curr;
+                dp[0][i][0]=dp[0][i-1][0]+curr;
                 
             }
             else{
-                dp[0][i][1]=max(dp[0][i-1][1]+coins[0][i],dp[0][i-1][2]);
-                dp[0][i][0]=max(dp[0][i-1][0]+coins[0][i],dp[0][i-1][1]);
+                dp[0][i][1]=max(dp[0][i-1][1]+curr,dp[0][i-1][2]);
+                dp[0][i][0]=max(dp[0][i-1][0]+curr,dp[0][i-1][1]);
             }
             
         
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(coins[i][j]<0){
-                    dp[i][j][2]=max(dp[i-1][j][2]+coins[i][j],dp[i][j-1][2]+coins[i][j]);
-                    dp[i][j][1]=max({dp[i-1][j][1]+coins[i][j],dp[i][j-1][1]+coins[i][j],dp[i-1][j][2],dp[i][j-1][2]});
-                    dp[i][j][0]=max({dp[i-1][j][0]+coins[i][j],dp[i][j-1][0]+coins[i][j],dp[i-1][j][1],dp[i][j-1][1]});
+                int curr=coins[i][j];
+                if(curr<0){
+                    dp[i][j][2]=max(dp[i-1][j][2]+curr,dp[i][j-1][2]+curr);
+                    dp[i][j][1]=max({dp[i-1][j][1]+curr,dp[i][j-1][1]+curr,dp[i-1][j][2],dp[i][j-1][2]});
+                    dp[i][j][0]=max({dp[i-1][j][0]+curr,dp[i][j-1][0]+curr,dp[i-1][j][1],dp[i][j-1][1]});
                 }
                 else{
-                    dp[i][j][2]=max(dp[i-1][j][2]+coins[i][j],dp[i][j-1][2]+coins[i][j]);
-                    dp[i][j][1]=max(dp[i-1][j][1]+coins[i][j],dp[i][j-1][1]+coins[i][j]);
-                    dp[i][j][0]=max(dp[i-1][j][0]+coins[i][j],dp[i][j-1][0]+coins[i][j]);
+                    dp[i][j][2]=max(dp[i-1][j][2]+curr,dp[i][j-1][2]+curr);
+                    dp[i][j][1]=max(dp[i-1][j][1]+curr,dp[i][j-1][1]+curr);
+                    dp[i][j][0]=max(dp[i-1][j][0]+curr,dp[i][j-1][0]+curr);
                     
                 }
             }

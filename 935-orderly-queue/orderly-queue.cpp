cfa69b266c5dca82f776719860f730cf8f8e34d8
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-06 02:52:19 +0530

    Time: 3 ms (62.18%) | Memory: 13.5 MB (34.36%) - LeetSync

---
+++ 935-orderly-queue/orderly-queue.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        int n=s.size();
+
+        string s_=s;
+        sort(s_.begin(),s_.end());
+        if(k>1)return s_;
+        string temp=s;
+        for(int i=0;i<n-1;i++){
+            string ref=s.substr(i+1)+s.substr(0,i+1);
+            if(temp>ref){
+                temp=ref;
+            }
+
+        }
+        return temp;
+    }
+};

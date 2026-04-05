1c060ad080d095b5f3f3788ed2ee0e7b8b293bef
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-05 16:13:05 +0530

    Time: 4 ms (27.83%) | Memory: 10.4 MB (47.26%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -4,10 +4,11 @@ public:
         int a=0;
         int b=0;
         for(int i=0;i<moves.size();i++){
-            if(moves[i]=='U')a++;
-            if(moves[i]=='D')a--;
-            if(moves[i]=='L')b++;
-            if(moves[i]=='R')b--;
+            int curr=moves[i];
+            if(curr=='U')a++;
+            if(curr=='D')a--;
+            if(curr=='L')b++;
+            if(curr=='R')b--;
             
         }
         if(a==0&&b==0)return true;

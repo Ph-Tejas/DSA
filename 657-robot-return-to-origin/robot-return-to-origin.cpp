a9aad1bd59a50d50bc16fbb8730cee16e4ebc6ad
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-05 16:13:40 +0530

    Time: 7 ms (10.20%) | Memory: 10.4 MB (47.26%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -3,12 +3,12 @@ public:
     bool judgeCircle(string moves) {
         int a=0;
         int b=0;
-        for(int i=0;i<moves.size();i++){
-            int curr=moves[i];
-            if(curr=='U')a++;
-            if(curr=='D')a--;
-            if(curr=='L')b++;
-            if(curr=='R')b--;
+        for(auto val:moves){
+            
+            if(val=='U')a++;
+            if(val=='D')a--;
+            if(val=='L')b++;
+            if(val=='R')b--;
             
         }
         if(a==0&&b==0)return true;

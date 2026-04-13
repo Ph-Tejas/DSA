bfd14d0536810a7cfd097e6ab54584509c187ae7
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-14 04:42:29 +0530

    Time: 132 ms (11.70%) | Memory: 293.3 MB (6.91%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,62 +1,61 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            int curr=nums[i];
-            
+        int n = nums.size();
+        vector<vector<int>> ans;
+        unordered_map<int,int> mp;  // changed
+
+        for(int i = 0; i < n; i++){
+            int curr = nums[i];
             mp[curr]++;
-            if(mp[curr]>2&&curr!=0)mp[curr]=2;
-
+            if(mp[curr] > 2 && curr != 0) mp[curr] = 2;
         }
-        vector<int>v;
-        if(mp[0]>2){
+
+        vector<int> v;
+        v.reserve(mp.size()); // added
+
+        if(mp[0] > 2){
             ans.push_back({0,0,0});
-            mp[0]=1;
+            mp[0] = 1;
         }
 
-        if(mp[0]==2)mp[0]=1;
+        if(mp[0] == 2) mp[0] = 1;
 
-        for(auto &val:mp){
-            if(val.second==2){
-                if(mp.find(-2*val.first)!=mp.end()){
-                    ans.push_back({val.first,val.first,-2*val.first});
+        for(auto &val : mp){
+            int x = val.first;  // added
+            if(val.second == 2){
+                if(mp.find(-2 * x) != mp.end()){
+                    ans.push_back({x, x, -2 * x});
                 }
-                val.second=1;
+                val.second = 1;
             }
-            if(val.second==1){
-                v.push_back(val.first);
+            if(val.second == 1){
+                v.push_back(x);
             }
         }
-        mp.clear();
-        vector<int>vis(2e5+1,-1);
-        n=v.size();
-        for(int i=0;i<n;i++){
-            vis[v[i]+1e5]=i;
+
+        vector<int> vis(200001, -1); // replaced 2e5+1
+        n = v.size();
+
+        for(int i = 0; i < n; i++){
+            vis[v[i] + 100000] = i;
         }
-        for(int i=0;i<n;i++){
-            
-            for(int j=i+1;j<n;j++){
-                int curr1=v[i];
-                int curr2=v[j];
-                int req=(-1*(curr1+curr2))+1e5;
-                if(req<=2e5 && req>-1){
-                    if(vis[req]>i && vis[req]>j){
-                        ans.push_back({curr1,curr2,req-100000});
+
+        for(int i = 0; i < n; i++){
+            int curr1 = v[i]; // moved out
+            for(int j = i + 1; j < n; j++){
+                int curr2 = v[j];
+                int req = (-1 * (curr1 + curr2)) + 100000;
+
+                if(req >= 0 && req <= 200000){
+                    int idx = vis[req]; // added
+                    if(idx > j){ // simplified condition
+                        ans.push_back({curr1, curr2, req - 100000});
                     }
                 }
             }
         }
+
         return ans;
-
-
-
-
-
-
-
-
     }
 };

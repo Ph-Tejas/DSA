9a49f1e70d579f55a0c24973620e309c6b92e873
Ph-Tejas <sharmatejas2005@gmail.com>
2026-04-17 06:07:01 +0530

    Time: 235 ms (10.55%) | Memory: 139.6 MB (10.21%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    int n,k;
+    void fun(int i,vector<int>l){
+        if(l.size()==k){
+            ans.push_back(l);
+            return;
+        }
+        if(n-i+1+l.size()<k)return;
+        for(int it=i;it<=n;it++){
+            l.push_back(it);
+            fun(it+1,l);
+            l.pop_back();
+        }
+        
+        
+        
+
+    }
+    vector<vector<int>> combine(int n_, int k_) {
+        n=n_;
+        k=k_;
+        fun(1,{});
+        return ans;
+        
+    }
+};

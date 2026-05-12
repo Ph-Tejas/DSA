f03aa1d5f997b81238d4fb0d619bd1e27c8d39b1
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-12 18:45:44 +0530

    Time: 103 ms (20.87%) | Memory: 110.5 MB (18.93%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+int n;
+    int fun(vector<vector<int>>& tasks,int sum){
+        int mini=0;
+        int maxi=sum;
+        int mid=mini+(maxi-mini)/2;
+        while(maxi>=mini){
+            int ct=mid;
+            bool flag=false;
+            for(int i=0;i<n;i++){
+                if(ct<tasks[i][1]){
+                    flag=true;
+                    break;
+                }
+                ct-=tasks[i][0];
+            }
+            if(flag){
+                mini=mid+1;
+            }
+            else{
+                maxi=mid-1;
+
+            }
+            mid=mini+(maxi-mini)/2;
+        }
+        return mini;
+    }
+    int minimumEffort(vector<vector<int>>& tasks) {
+
+        n=tasks.size();
+        for(int i=0;i<n;i++){
+            tasks[i].push_back(tasks[i][1]-tasks[i][0]);
+        }
+        sort(tasks.begin(),tasks.end(),[](vector<int>&a,vector<int>&b){
+            if(a[2]==b[2]){
+                if(a[1]==b[1]){
+                    return a[0]<b[0];
+                }
+                return a[1]>b[1];
+            }
+            return a[2]>b[2];
+        });
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=tasks[i][1];
+        }
+        
+        return fun(tasks,sum);
+    }
+};

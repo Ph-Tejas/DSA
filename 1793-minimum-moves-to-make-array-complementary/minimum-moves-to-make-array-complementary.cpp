91110358d045653c6951d6ab236623d419f5d86e
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-13 19:47:41 +0530

    Time: 163 ms (18.58%) | Memory: 204.1 MB (19.47%) - LeetSync

---
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int n=nums.size();
+        vector<int>v(200002);
+        map<int,int>mp;
+        for(int i=0;i<n/2;i++){
+            int a=nums[i];
+            int b=nums[n-1-i];
+            mp[a+b]++;
+            v[min(a,b)+1]++;
+            v[max(a,b)+limit+1]--;
+        }
+        for(int i=1;i<200002;i++){
+            v[i]=v[i-1]+v[i];
+        }
+
+        int ans=n/2;
+        for(auto val:mp){
+            int a=v[val.first]-val.second;
+            int c=(n/2)-v[val.first];
+            ans=min(ans,(a+2*c));
+
+        }
+        return ans;
+        
+        
+
+    }
+};

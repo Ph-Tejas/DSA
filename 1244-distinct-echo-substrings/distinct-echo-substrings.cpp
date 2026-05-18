eb63c92bf6bbd6f9e713406ff6029a0c5d8a9e1c
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-18 20:22:58 +0530

    Time: 183 ms (46.71%) | Memory: 209.3 MB (19.08%) - LeetSync

--- 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
+++ 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
@@ -1,110 +1,76 @@
 class Solution {
 public:
-    long long mod = 1e9 + 7;
+    long long mod=1e9+7;
+    long long powy(long long a,long long b){
+        long long res=1;
 
-    long long powy(long long a, long long b) {
-        long long res = 1;
-
-        while (b) {
-            if (b & 1) {
-                res = (res * a) % mod;
-            }
-
-            a = (a * a) % mod;
-            b >>= 1;
+        while(b){
+            if(b&1) res=(res*a)%mod;
+            a=(a*a)%mod;
+            b>>=1;
         }
 
         return res;
     }
-
-    long long getHash(
-        int l,
-        int r,
-        vector<long long>& pref,
-        vector<long long>& invPower
-    ) {
-
-        long long val = pref[r];
-
-        if (l > 0) {
-            val = (val - pref[l - 1] + mod) % mod;
-        }
-
-        return (val * invPower[l]) % mod;
-    }
-
     int distinctEchoSubstrings(string text) {
-
-        int n = text.size();
-
-        vector<long long> power(n);
+        int n=text.size();
+        vector<vector<long long>>rpr(n,vector<long long>(n));
+        vector<int> ans;
+        vector<long long>power(n);
+        for(int i=0;i<n;i++){
+            power[i]=powy(27,i)%mod;
+        }
+        
         vector<long long> invPower(n);
 
-        power[0] = 1;
-
-        for (int i = 1; i < n; i++) {
-            power[i] = (power[i - 1] * 27) % mod;
+        for(int i=0;i<n;i++){
+            power[i]=powy(27,i)%mod;
+            invPower[i]=powy(power[i],mod-2)%mod;
         }
-
-        long long inv27 = powy(27, mod - 2);
-
-        invPower[0] = 1;
-
-        for (int i = 1; i < n; i++) {
-            invPower[i] = (invPower[i - 1] * inv27) % mod;
-        }
-
-        vector<long long> pref(n);
-
-        for (int i = 0; i < n; i++) {
-
-            long long val = (text[i] - 'a' + 1);
-
-            val = (val * power[i]) % mod;
-
-            pref[i] = val;
-
-            if (i > 0) {
-                pref[i] = (pref[i] + pref[i - 1]) % mod;
+        vector<long long>cWise(n);
+        for(int i=0;i<n;i++){
+            cWise[i]=1+(text[i]-'a');
+            if(i!=0){
+                cWise[i]=((((cWise[i]%mod)*(power[i]%mod))%mod)+(cWise[i-1]%mod))%mod;
             }
         }
-
-        unordered_set<long long> fin;
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = i; j < n; j++) {
-
-                int len = j - i + 1;
-
-                long long curHash = getHash(i, j, pref, invPower);
-
-                for (int it = 1;; it++) {
-
-                    int nl = i + (it * len);
-                    int nr = j + (it * len);
-
-                    if (nr >= n) {
-                        break;
-                    }
-
-                    long long nextHash =
-                        getHash(nl, nr, pref, invPower);
-
-                    if (nextHash != curHash) {
-                        break;
-                    }
-
-                    if (it & 1) {
-
-                        fin.insert(
-                            getHash(i, nr, pref, invPower)
-                        );
-                    }
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(i==0){
+                    rpr[i][j]=cWise[j];
                 }
+                else rpr[i][j]=(((cWise[j]-cWise[i-1]+mod)%mod)*invPower[i])%mod;
+                
+                
             }
         }
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int ln=j-i+1;
 
+                for(int it=1;it<2;it++){
+                    if(j+(it*ln)>=n || rpr[(i+(it*ln))][j+(it*ln)]!=rpr[i][j])break;
+
+                
+                    if(it&1)ans.push_back(rpr[i][j+(it*ln)]);
+                    
+                    
+                }
+
+
+            }
+        }
+        unordered_set<int>fin;
+        for(int i=0;i<ans.size();i++){
+           
+            fin.insert(ans[i]);
+        }
         return fin.size();
+
+
+
+
+
+
     }
 };

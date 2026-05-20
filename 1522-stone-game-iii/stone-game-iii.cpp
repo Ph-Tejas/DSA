0d78a19ff301277067d85a5f4cd474aa2bd2c2b2
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-20 16:46:49 +0530

    Time: 8 ms (92.33%) | Memory: 136.1 MB (70.33%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>>dp;
-    int fun(int guy,int ind,vector<int>& nums){
-        if(ind==n)return 0;
-        if(dp[guy][ind]!=-1)return dp[guy][ind];
-        int mul=1;
-        if(guy==1)mul*=-1;
-        int curr=0;
-        int ans;
-        if(guy==0)ans=INT_MIN;
-        else ans=INT_MAX;
-        for(int i=0;i<3;i++){
-            if(ind+i==n)break;
-            curr+=nums[ind+i];
-            if(guy==0)ans=max(ans,curr*mul+fun(!guy,ind+i+1,nums));
-            else ans=min(ans,curr*mul+fun(!guy,ind+i+1,nums));
-        }
-        return dp[guy][ind]=ans;
-    }
     string stoneGameIII(vector<int>& nums) {
-        n=nums.size();
-        dp.resize(2,vector<int>(n,-1));
-        int ans=fun(0,0,nums);
-        if(ans>0)return "Alice";
-        else if(ans<0)return "Bob";
-        return "Tie";
+        
+        int n = nums.size();
 
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int take = 0;
+
+            dp[i] = INT_MIN;
+
+            for (int k = 0; k < 3 && i + k < n; k++) {
+
+                take += nums[i + k];
+
+                dp[i] = max(dp[i], take - dp[i + k + 1]);
+            }
+        }
+
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+
+        return "Tie";
     }
 };

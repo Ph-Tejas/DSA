7e1559372ed8436f39b1c58fbb55fa1cec8c2508
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-22 02:03:33 +0530

    Time: 251 ms (8.36%) | Memory: 325.3 MB (5.08%) - LeetSync

--- 732-my-calendar-iii/my-calendar-iii.cpp
+++ 732-my-calendar-iii/my-calendar-iii.cpp
@@ -1,126 +1,104 @@
 class MyCalendarThree {
 public:
-    vector<pair<int,int>>ranges;
-    vector<int>ct;
-    MyCalendarThree() {
-        
-    }
-    
+    vector<pair<int,int>> ranges;
+    vector<int> ct;
+
+    MyCalendarThree() {}
+
     int book(int startTime, int endTime) {
         endTime--;
-        
-        vector<pair<int,int>>r;
-        vector<int>c;
-        if(ranges.empty()){
-            ranges.push_back({startTime,endTime});
+
+        vector<pair<int,int>> r;
+        vector<int> c;
+
+        if (ranges.empty()) {
+            ranges.push_back({startTime, endTime});
             ct.push_back(1);
             return 1;
         }
-        int ans=1;
-        bool flag=false;
+
+        int ans = 1;
+        bool flag = false;
         int n = ranges.size();
-        for(int i=0;i<n;i++){
-            int rs=ranges[i].first;
-            int re=ranges[i].second;
-            if(flag){
+
+        for (int i = 0; i < n; i++) {
+            int rs = ranges[i].first;
+            int re = ranges[i].second;
+
+            if (flag) {
                 r.push_back(ranges[i]);
                 c.push_back(ct[i]);
-                ans=max(ans,ct[i]);
+                ans = max(ans, ct[i]);
                 continue;
             }
-            if(rs>endTime){
-                // cout<<1;
-                r.push_back({startTime,endTime});
+
+            if (rs > endTime) {
+                r.push_back({startTime, endTime});
                 c.push_back(1);
                 r.push_back(ranges[i]);
                 c.push_back(ct[i]);
-                ans=max(ans,ct[i]);
-
-                flag=true;
+                ans = max(ans, ct[i]);
+                flag = true;
             }
-            else if(rs>startTime && rs<=endTime && re>=endTime){
-                // cout<<2;
-                r.push_back({startTime,rs-1});
+            else if (rs > startTime && rs <= endTime && re >= endTime) {
+                r.push_back({startTime, rs - 1});
                 c.push_back(1);
-                r.push_back({rs,endTime});
-                c.push_back(ct[i]+1);
-                ans=max(ans,ct[i]+1);
-                if(endTime!=re){
-                    r.push_back({endTime+1,re});
+                r.push_back({rs, endTime});
+                c.push_back(ct[i] + 1);
+                ans = max(ans, ct[i] + 1);
+                if (endTime != re) {
+                    r.push_back({endTime + 1, re});
                     c.push_back(ct[i]);
-                    ans=max(ans,ct[i]);
                 }
-                flag=true;
+                flag = true;
             }
-            else if(rs>startTime && re<endTime){
-                // cout<<3;
-                r.push_back({startTime,rs-1});
+            else if (rs > startTime && re < endTime) {
+                r.push_back({startTime, rs - 1});
                 c.push_back(1);
-                r.push_back({rs,re});
-                c.push_back(ct[i]+1);
-
-                ans=max(ans,ct[i]+1);
-                startTime=re+1;
-
+                r.push_back({rs, re});
+                c.push_back(ct[i] + 1);
+                ans = max(ans, ct[i] + 1);
+                startTime = re + 1;
             }
-            else if(rs<=startTime && startTime<=re && re>=endTime){
-                // cout<<4;
-                if(rs!=startTime){
-                    r.push_back({rs,startTime-1});
+            else if (rs <= startTime && startTime <= re && re >= endTime) {
+                if (rs != startTime) {
+                    r.push_back({rs, startTime - 1});
                     c.push_back(ct[i]);
-
-                    ans=max(ans,ct[i]);
                 }
-                r.push_back({startTime,endTime});
-                c.push_back(ct[i]+1);
-
-                ans=max(ans,ct[i]+1);
-                if(re!=endTime){
-                    r.push_back({endTime+1,re});
+                r.push_back({startTime, endTime});
+                c.push_back(ct[i] + 1);
+                ans = max(ans, ct[i] + 1);
+                if (re != endTime) {
+                    r.push_back({endTime + 1, re});
                     c.push_back(ct[i]);
-
-                    ans=max(ans,ct[i]);
                 }
-                flag=true;
-
+                flag = true;
             }
-            else if(rs<=startTime && startTime<=re && re<endTime){
-                // cout<<5;
-                if(rs!=startTime){
-                    r.push_back({rs,startTime-1});
+            else if (rs <= startTime && startTime <= re && re < endTime) {
+                if (rs != startTime) {
+                    r.push_back({rs, startTime - 1});
                     c.push_back(ct[i]);
-
-                    ans=max(ans,ct[i]);
                 }
-                r.push_back({startTime,re});
-                c.push_back(ct[i]+1);
-                ans=max(ans,ct[i]+1);
-                startTime=re+1;
-
+                r.push_back({startTime, re});
+                c.push_back(ct[i] + 1);
+                ans = max(ans, ct[i] + 1);
+                startTime = re + 1;
             }
-            else{
+            else {
                 r.push_back(ranges[i]);
                 c.push_back(ct[i]);
-                ans=max(ans,ct[i]);
+                ans = max(ans, ct[i]);
             }
-            
-            
-
         }
-        r.push_back({startTime,endTime});
-        c.push_back(1);
-        ranges=move(r);
-        ct=move(c);
+
+        // ✅ Only append remaining [startTime,endTime] if not yet placed
+        if (!flag) {
+            r.push_back({startTime, endTime});
+            c.push_back(1);
+        }
+
+        ranges = move(r);
+        ct = move(c);
         return ans;
-
-
-
-
     }
 };
-
-/**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(startTime,endTime);
- */

d687499a3088197aa3c277ad09fadceb7a5cad68
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-24 04:59:02 +0530

    Time: 21 ms (34.90%) | Memory: 25.4 MB (59.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n=strs.size();
         vector<string>v=strs;
-        map<string,vector<int>>mp;
+        unordered_map<string,vector<int>>mp;
         for(int i=0;i<n;i++){
             sort(v[i].begin(),v[i].end());
             mp[v[i]].push_back(i);

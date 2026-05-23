12fb81c5d6e0b1d0212a59077c5146ba03d31cba
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-23 17:11:56 +0530

    Time: 1009 ms (13.75%) | Memory: 155.2 MB (9.75%) - LeetSync

---
+++ 1881-closest-subsequence-sum/closest-subsequence-sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    void create(vector<int>&v,int &sz1,int i,int sum,vector<int>&sum1){
+        if(i==sz1){
+            sum1.push_back(sum);
+            return;
+        }
+        int curr=v[i];
+        sum1.push_back(sum+v[i]);
+        create(v,sz1,i+1,sum+v[i],sum1);
+        create(v,sz1,i+1,sum,sum1);
+
+
+
+    }
+
+    int minAbsDifference(vector<int>& nums, int goal) {
+        int n=nums.size();
+        vector<int>v1;
+        vector<int>v2;
+        vector<int>sum1,sum2;
+        for(int i=0;i<n/2;i++){
+            v1.push_back(nums[i]);
+        }
+        for(int i=n/2;i<n;i++){
+            v2.push_back(nums[i]);
+        }
+        int sz1=v1.size();
+        int sz2=v2.size();
+        create(v1,sz1,0,0,sum1);
+        create(v2,sz2,0,0,sum2);
+        sort(sum2.begin(),sum2.end());
+        int mini=abs(goal);
+        for(int i=0;i<sum1.size();i++){
+            int curr=sum1[i];
+            auto it=lower_bound(sum2.begin(),sum2.end(),goal-curr);
+            
+            if(it!=sum2.end())mini=min(mini,abs((*it)+curr-goal));
+            if(it!=sum2.begin()){
+                it--;
+            }
+            mini=min(mini,abs((*it)+curr-goal));
+
+        }
+        return mini;
+
+        
+
+    }
+};

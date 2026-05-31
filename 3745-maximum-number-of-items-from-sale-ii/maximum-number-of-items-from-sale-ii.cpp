b1df5428f9a14218d98b2f291e2d5ba6ee822028
Ph-Tejas <sharmatejas2005@gmail.com>
2026-05-31 18:12:01 +0530

    Time: 247 ms (19.21%) | Memory: 269.9 MB (37.63%) - LeetSync

--- 3745-maximum-number-of-items-from-sale-ii/maximum-number-of-items-from-sale-ii.cpp
+++ 3745-maximum-number-of-items-from-sale-ii/maximum-number-of-items-from-sale-ii.cpp
@@ -1,95 +1,94 @@
 class Solution {
 public:
+    struct Node {
+        double cost;
+        int power;
+        int price;
+        int idx;
+    };
+
     int maximumSaleItems(vector<vector<int>>& items, int budget) {
-        int n=items.size();
-        vector<int>freq(n+1);
-        for(int i=0;i<n;i++){
+        int n = items.size();
+
+        int mx = 0;
+        for (auto &x : items) {
+            mx = max(mx, x[0]);
+        }
+
+        vector<int> freq(mx + 1, 0);
+
+        for (int i = 0; i < n; i++) {
             freq[items[i][0]]++;
         }
-        vector<int>possible(n+1);
-        vector<vector<float>>v;
-        int ct=0;
-        for(int i=1;i<=n;i++){
-            if(freq[i]==0)continue;
-            ct=0;
-            for(int j=i;j<=n;j+=i){
-                if(freq[j]==0){
-                    continue;
-                }
-                ct+=freq[j];
 
+        vector<int> possible(mx + 1, 0);
 
+        for (int i = 1; i <= mx; i++) {
+            if (freq[i] == 0) continue;
+
+            int ct = 0;
+
+            for (int j = i; j <= mx; j += i) {
+                ct += freq[j];
             }
-            possible[i]=ct-1;
-        }
-        vector<int>available(n);
-        for(int i=0;i<n;i++){
-            available[i]=possible[items[i][0]];
-            float price=items[i][1];
-            v.push_back({price,1,price,(float)i});
-            if(available[i]!=0)v.push_back({(price)/2,2,price,(float)i});
-            
-        }
-        sort(v.begin(),v.end(),[](vector<float>&a,vector<float>&b){
-            if(a[0]==b[0]){
-                if(a[1]==b[1]){
 
-                    return a[2]<b[2];
-                }
-                return a[1]>b[1];
+            possible[i] = ct - 1;
+        }
 
+        vector<int> available(n);
+
+        vector<Node> v;
+        v.reserve(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            available[i] = possible[items[i][0]];
+
+            int price = items[i][1];
+
+            v.push_back({(double)price, 1, price, i});
+
+            if (available[i] > 0) {
+                v.push_back({price / 2.0, 2, price, i});
             }
-            return a[0]<b[0];
+        }
+
+        sort(v.begin(), v.end(), [](const Node& a, const Node& b) {
+            if (a.cost != b.cost)
+                return a.cost < b.cost;
+
+            if (a.power != b.power)
+                return a.power > b.power;
+
+            return a.price < b.price;
         });
-        for(int i=0;i<available.size();i++){
-            // cout<<available[i]<<endl;
-        }
-        for(int i=0;i<v.size();i++){
-            // cout<<v[i][0]<<" "<<v[i][1]<<" "<<v[i][2]<<" "<<v[i][3]<<endl;
-        }
-        int it=0;
-        int ans=0;
-        int sz=v.size();
-        while(it<sz){
-            // cout<<budget<<endl;
 
-            long long power=v[it][1];
-            long long price=v[it][2];
-            long long ind=v[it][3];
-            if(power==2){
-                if(price<=budget){
-                    long long maxima=available[ind]*price;
-                    if(maxima<=budget){
-                        ans+=available[ind]*power;
-                        budget-=maxima;
-                    }
-                    else{
-                        int can=budget/price;
-                        budget-=can*price;
-                        ans+=can*power;
-                    }
+        int ans = 0;
 
+        for (auto &cur : v) {
+            long long power = cur.power;
+            long long price = cur.price;
+            int idx = cur.idx;
+
+            if (power == 2) {
+                if (price > budget) continue;
+
+                long long avail = available[idx];
+                long long totalCost = avail * price;
+
+                if (totalCost <= budget) {
+                    ans += avail * 2;
+                    budget -= totalCost;
+                } else {
+                    long long can = budget / price;
+                    ans += can * 2;
+                    budget -= can * price;
                 }
-                it++;
-            }
-            else{
-                int can=budget/price;
-                // cout<<can;
-                ans+=can;
+            } else {
+                ans += budget / price;
                 break;
             }
         }
+
         return ans;
-
-
-
-
-
-
-
-
-
-
-
     }
 };

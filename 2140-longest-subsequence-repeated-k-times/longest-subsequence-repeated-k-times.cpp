13a9c0302eecb9954a8e942d0d963dc9311ccefe
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-02 20:46:31 +0530

    Time: 462 ms (32.08%) | Memory: 59.3 MB (20.75%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -1,98 +1,114 @@
 class Solution {
 public:
-    vector<bool>vis;
+    vector<bool> vis;
 
-    void build(string s,vector<string>&toCheck,int &sz,vector<string>&available){
-        
-        
-        if(count(vis.begin(),vis.end(),true)==sz)return;
+    // CHANGED:
+    // 1) string passed by reference instead of by value
+    // 2) added "used" parameter so we don't call count(vis.begin(), vis.end(), true)
+    void build(string &cur,
+               vector<string> &toCheck,
+               int &sz,
+               vector<string> &available,
+               int used) {
 
-        for(int j=0;j<sz;j++){
-            if(vis[j])continue;
-            s.push_back(available[j][0]);
-            toCheck.push_back(s);
-            vis[j]=true;
-            build(s,toCheck,sz,available);
-            s.pop_back();
-            vis[j]=false;
+        if (used == sz) return;
 
+        for (int j = 0; j < sz; j++) {
+            if (vis[j]) continue;
+
+            cur.push_back(available[j][0]);
+
+            toCheck.push_back(cur);
+
+            vis[j] = true;
+            build(cur, toCheck, sz, available, used + 1);
+            vis[j] = false;
+
+            cur.pop_back();
         }
-
     }
 
     string longestSubsequenceRepeatedK(string s, int k) {
-        
-        int n=s.size();
-        
-        vector<int>freq(26);
-        for(int i=0;i<n;i++){
-            freq[s[i]-'a']++;
+
+        int n = (int)s.size();
+
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
         }
-        vector<string>available;
-        for(int i=0;i<26;i++){
-            if(freq[i]>=k){
-                while(freq[i]/k!=0){
+
+        vector<string> available;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] >= k) {
+
+                // same logic as your code
+                while (freq[i] / k != 0) {
                     string str;
-                    str.push_back(i+'a');
+                    str.push_back(char(i + 'a'));
                     available.push_back(str);
-                    freq[i]-=k;
+                    freq[i] -= k;
                 }
             }
-
         }
-        int sz=available.size();
-        vis.resize(sz,false);
-        vector<string>toCheck;
-        for(int i=0;i<sz;i++){
+
+        int sz = (int)available.size();
+
+        vis.assign(sz, false);
+
+        vector<string> toCheck;
+
+        // CHANGED:
+        // reserve some space to reduce reallocations
+        toCheck.reserve(50000);
+
+        for (int i = 0; i < sz; i++) {
+
             toCheck.push_back(available[i]);
-            vis[i]=true;
-            build(available[i],toCheck,sz,available);
-            vis[i]=false;
+
+            vis[i] = true;
+
+            string cur = available[i];
+
+            // CHANGED:
+            // start with 1 used character
+            build(cur, toCheck, sz, available, 1);
+
+            vis[i] = false;
         }
 
-        sort(toCheck.begin(),toCheck.end(),[](string &a,string &b){
-            if(a.size()==b.size()){
-                return a>b;
-            }
-            return a.size()>b.size();
-        });
-        sz=toCheck.size();
-        for(int i=0;i<sz;i++){
-            int szz=toCheck[i].size();
-            int it=0;
-            int ct=0;
-            for(int j=0;j<n;j++){
-                if(s[j]==toCheck[i][it]){
-                    it++;
-                    if(it==szz){
-                        ct++;
-                        it=0;
-                    }
+        sort(toCheck.begin(), toCheck.end(),
+             [](const string &a, const string &b) {
+                 if (a.size() == b.size())
+                     return a > b;
+                 return a.size() > b.size();
+             });
 
+        sz = (int)toCheck.size();
+
+        for (int i = 0; i < sz; i++) {
+
+            int len = (int)toCheck[i].size();
+
+            int it = 0;
+            int ct = 0;
+
+            for (int j = 0; j < n; j++) {
+
+                if (s[j] == toCheck[i][it]) {
+                    it++;
+
+                    if (it == len) {
+                        ct++;
+                        it = 0;
+                    }
                 }
             }
-            if(ct>=k)return toCheck[i];
 
+            if (ct >= k)
+                return toCheck[i];
         }
+
         return "";
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 };

04c38ea093520dc3806ad83a1899aac1d5f75d17
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-05 19:26:37 +0530

    Time: 795 ms (5.93%) | Memory: 287.9 MB (5.08%) - LeetSync

---
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.cpp
@@ -0,0 +1,167 @@
+class Solution {
+public:
+    int n;
+    long long stoii(string s){
+        long long mul=1;
+        int nz=s.size();
+        long long ans=0;
+        for(int i=nz-1;i>-1;i--){
+            ans+=(s[i]-'0')*mul;
+            mul*=10;
+        }
+        return ans;
+    }
+    long long LetsDoIt(string &s,int sz,int prev,int tight,int shouldAdd,int didStart,vector<vector<vector<vector<vector<long long>>>>>&dp){
+        if(sz==0)return 0;
+        if(dp[sz][prev][tight][shouldAdd][didStart]!=-1)return dp[sz][prev][tight][shouldAdd][didStart];
+        int curr=s[n-sz]-'0';
+        long long ans=0;
+        int it=0;
+        if(tight){
+            for(it;it<min(prev,curr+1);it++){
+                
+                if(shouldAdd==2){
+                    if(it!=curr){
+                        ans+=pow(10,sz-1);
+                    }
+                    else{
+                        if(n-sz+1!=n){
+                            
+                            string str=s.substr(n-sz+1);
+                            long long state=stoii(str);
+                            ans+=state+1;
+                        }
+                        else ans++;
+                    }
+                }
+                ans+=LetsDoIt(s,sz-1,it,(it==curr)?1:0,(didStart==1)?1:0,((didStart)||(!didStart && it!=0))?1:0,dp);
+            }
+            if(it!=curr+1){
+                if(it==prev){
+                    ans+=LetsDoIt(s,sz-1,it,(it==curr)?1:0,0,((didStart)||(!didStart && it!=0))?1:0,dp);
+                    it++;
+                }
+                for(it;it<curr+1;it++){
+                    if(shouldAdd==1){
+                        if(it!=curr){
+                            ans+=pow(10,sz-1);
+                        }
+                        else{
+                            if(n-sz+1!=n){
+                                string str=s.substr(n-sz+1);
+                                long long state=stoii(str);
+                                ans+=state+1;
+                            }
+                            else ans++;
+                        }
+                    }
+                    ans+=LetsDoIt(s,sz-1,it,(it==curr)?1:0,(didStart==1)?2:0,((didStart)||(!didStart && it!=0))?1:0,dp);
+                }
+            }
+        }
+        else{
+            curr=9;
+            for(it;it<min(prev,curr+1);it++){
+                if(shouldAdd==2){
+                    ans+=pow(10,sz-1);
+                    
+                }
+                ans+=LetsDoIt(s,sz-1,it,0,(didStart==1)?1:0,((didStart)||(!didStart && it!=0))?1:0,dp);
+            }
+            if(it!=curr+1){
+                if(it==prev){
+                    ans+=LetsDoIt(s,sz-1,it,0,0,((didStart)||(!didStart && it!=0))?1:0,dp);
+                    it++;
+                }
+                for(it;it<curr+1;it++){
+                    if(shouldAdd==1){
+                        ans+=pow(10,sz-1);
+                    }
+                    ans+=LetsDoIt(s,sz-1,it,0,(didStart==1)?2:0,((didStart)||(!didStart && it!=0))?1:0,dp);
+                }
+            }
+        }
+        return dp[sz][prev][tight][shouldAdd][didStart]=ans;
+            
+
+
+
+    }
+    long long fun(long long num,vector<vector<vector<vector<vector<long long>>>>>&dp){
+        string s=to_string(num);
+        n=s.size();
+
+        
+
+
+        int sz=s.size();
+        if(n<3)return 1LL*0;
+
+        return LetsDoIt(s,sz,0,1,0,0,dp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    long long totalWaviness(long long num1, long long num2) {
+        
+        
+    //sz=16, prev= 10, tight=2, shouldAdd=3, didStart=2;
+        vector<vector<vector<vector<vector<long long>>>>>dp1,dp2;
+        dp1.resize(20,vector<vector<vector<vector<long long>>>>(10,vector<vector<vector<long long>>>(2,vector<vector<long long>>(3,vector<long long>(2,-1)))));
+        
+        dp2.resize(20,vector<vector<vector<vector<long long>>>>(10,vector<vector<vector<long long>>>(2,vector<vector<long long>>(3,vector<long long>(2,-1)))));
+        return fun(num2,dp1)-fun(num1-1,dp2);
+    }
+};

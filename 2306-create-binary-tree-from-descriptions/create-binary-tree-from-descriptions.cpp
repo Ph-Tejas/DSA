1ca53e571168a7de19e5cff62b02a3d27a6a4395
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-07 23:39:19 +0530

    Time: 488 ms (5.26%) | Memory: 297.2 MB (20.23%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& description) {
+        int n=description.size();
+        map<int,TreeNode*>mp;
+        set<int>st;
+        for(int i=0;i<n;i++){
+            int curr=description[i][1];
+            mp[curr]=new TreeNode(curr);
+            mp[description[i][0]]=new TreeNode(description[i][0]);
+            st.insert(curr);
+            st.insert(description[i][0]);
+        }for(int i=0;i<n;i++){
+            int curr=description[i][1];
+
+            if(st.find(curr)!=st.end())st.erase(curr);
+            if(description[i][2]==0)mp[description[i][0]]->right=mp[curr];
+            else mp[description[i][0]]->left=mp[curr];
+        }
+        auto it=st.begin();
+        return mp[(*it)];
+        
+
+    }
+};

de44e0a9ae46d6b6bacd39f6473a865ab3263a05
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-08 17:59:48 +0530

    Time: 387 ms (47.52%) | Memory: 189.4 MB (35.45%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -1,91 +1,70 @@
 class Solution {
 public:
-    int maxi=0;
-    vector<vector<int>>dp;
-    vector<vector<int>>adj;
-    vector<bool>done;
-    vector<bool>vis;
-    vector<int>fun(int node,string &colors){
-        if(vis[node]){
-            
-            return dp[node]={(int)1e9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        }
-        if(done[node]){
+    int n;
+    bool cycle = false;
+
+    vector<vector<int>> dp;
+    vector<vector<int>> adj;
+
+    // 0 = unvisited
+    // 1 = processing
+    // 2 = finished
+    vector<int> state;
+
+    vector<int>& dfs(int node, string &colors) {
+        if(state[node] == 1) {
+            cycle = true;
             return dp[node];
         }
-        vis[node]=true;
-        vector<int>ans(26);
-        for(int j=0;j<adj[node].size();j++){
 
-            for(int k=0;k<26;k++){
-                
+        if(state[node] == 2) {
+            return dp[node];
+        }
 
-                if(k==colors[node]-'a'){
-                    if(done[adj[node][j]]){
-                        ans[k]=max(ans[k],1+dp[adj[node][j]][k]);
-                    }
-                    
-                    else ans[k]=max(ans[k],1+fun(adj[node][j],colors)[k]);
-                }
-                else{
-                    if(done[adj[node][j]]){
-                        ans[k]=max(ans[k],dp[adj[node][j]][k]);
-                    }
-                    else ans[k]=max(ans[k],fun(adj[node][j],colors)[k]);
-                }
+        state[node] = 1;
 
+        for(int v : adj[node]) {
+            vector<int>& child = dfs(v, colors);
 
-
+            for(int c = 0; c < 26; c++) {
+                dp[node][c] = max(dp[node][c], child[c]);
             }
-            
-
-
         }
-        if(adj[node].size()==0){
-            ans[colors[node]-'a']=1;
-        }
-        vis[node]=false;
-        done[node]=true;
-        return dp[node]=ans;
 
-        
+        dp[node][colors[node] - 'a']++;
+
+        state[node] = 2;
+
+        return dp[node];
     }
 
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n=colors.size();
-        int sz=edges.size();
+        n = colors.size();
 
-        dp.resize(n,vector<int>(26,-1));
-        vis.resize(n,false);
-        adj.resize(n);
-        done.resize(n,false);
-        for(int i=0;i<sz;i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
+        adj.assign(n, {});
+        dp.assign(n, vector<int>(26, 0));
+        state.assign(n, 0);
+
+        for(auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
         }
-        for(int i=0;i<n;i++){
-            
-            
-            int mx=0;
-            for(int k=0;k<26;k++){
-                if(done[i]){
-                    mx=max(dp[i][k],mx);
-                }
-                else mx=max(mx,fun(i,colors)[k]);
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(state[i] == 0) {
+                dfs(i, colors);
             }
-            maxi=max(mx,maxi);
-
-            
         }
-        return ((maxi>=1e9)?-1:maxi);
 
+        if(cycle) return -1;
 
+        for(int i = 0; i < n; i++) {
+            for(int c = 0; c < 26; c++) {
+                ans = max(ans, dp[i][c]);
+            }
+        }
 
-
-
-
-
-
-
-
+        return ans;
     }
 };

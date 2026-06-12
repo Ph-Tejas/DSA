c25bb970b806e8a60b1059480f216dead26004a2
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-12 22:32:18 +0530

    Time: 40 ms (95.81%) | Memory: 21.3 MB (95.99%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -3,67 +3,7 @@ public:
     int openLock(vector<string>& deadends, string target) {
         int n=1e4;
         vector<int>powy={1,10,100,1000};
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++){
-            int num=i;
-            int ch1=num/powy[3];
-            if(ch1==0){
-                adj[i].push_back(i+1000);
-                adj[i].push_back(i+9000);
-            }
-            else if(ch1==9){
-                adj[i].push_back(i-1000);
-                adj[i].push_back(i-9000);
-                
-            }
-            else{
-                adj[i].push_back(i-1000);
-                adj[i].push_back(i+1000);
-            }
-            int ch2=(num/powy[2])-(ch1*10);
-            if(ch2==0){
-                adj[i].push_back(i+100);
-                adj[i].push_back(i+900);
-            }
-            else if(ch2==9){
-                adj[i].push_back(i-100);
-                adj[i].push_back(i-900);
-                
-            }
-            else{
-                adj[i].push_back(i-100);
-                adj[i].push_back(i+100);
-            }
-            int ch3=(num/powy[1])-(ch1*100+ch2*10);
-            if(ch3==0){
-                adj[i].push_back(i+10);
-                adj[i].push_back(i+90);
-            }
-            else if(ch3==9){
-                adj[i].push_back(i-10);
-                adj[i].push_back(i-90);
-                
-            }
-            else{
-                adj[i].push_back(i-10);
-                adj[i].push_back(i+10);
-            }
-            int ch4=(num/powy[0])-(ch1*1000+ch2*100+ch3*10);
-            if(ch4==0){
-                adj[i].push_back(i+1);
-                adj[i].push_back(i+9);
-            }
-            else if(ch4==9){
-                adj[i].push_back(i-1);
-                adj[i].push_back(i-9);
-                
-            }
-            else{
-                adj[i].push_back(i-1);
-                adj[i].push_back(i+1);
-         
-            }
-        }
+        
         vector<bool>vis(n);
         for(int i=0;i<deadends.size();i++){
             vis[stoi(deadends[i])]=true;
@@ -71,16 +11,68 @@ public:
         queue<pair<int,int>>q;
         q.push({0,0});
         while(!q.empty()){
-            int node=q.front().first;
+            int i=q.front().first;
             int level=q.front().second;
 
             q.pop();
-            if(stoi(target)==node)return level;
-            if(vis[node])continue;
-            vis[node]=true;
-            for(int i=0;i<adj[node].size();i++){
-                if(vis[adj[node][i]])continue;
-                q.push({adj[node][i],level+1});
+            if(stoi(target)==i)return level;
+            if(vis[i])continue;
+            vis[i]=true;
+            int ch1=i/powy[3];
+            if(ch1==0){
+                if(!vis[i+1000])q.push({i+1000,level+1});
+                if(!vis[i+9000])q.push({i+9000,level+1});
+            }
+            else if(ch1==9){
+                if(!vis[i-1000])q.push({i-1000,level+1});
+                if(!vis[i-9000])q.push({i-9000,level+1});
+                
+            }
+            else{
+                if(!vis[i-1000])q.push({i-1000,level+1});
+                if(!vis[i+1000])q.push({i+1000,level+1});
+            }
+            int ch2=(i/powy[2])-(ch1*10);
+            if(ch2==0){
+                if(!vis[i+100])q.push({i+100,level+1});
+                if(!vis[i+900])q.push({i+900,level+1});
+            }
+            else if(ch2==9){
+                if(!vis[i-100])q.push({i-100,level+1});
+                if(!vis[i-900])q.push({i-900,level+1});
+                
+            }
+            else{
+                if(!vis[i-100])q.push({i-100,level+1});
+                if(!vis[i+100])q.push({i+100,level+1});
+            }
+            int ch3=(i/powy[1])-(ch1*100+ch2*10);
+            if(ch3==0){
+                if(!vis[i+10])q.push({i+10,level+1});
+                if(!vis[i+90])q.push({i+90,level+1});
+            }
+            else if(ch3==9){
+                if(!vis[i-10])q.push({i-10,level+1});
+                if(!vis[i-90])q.push({i-90,level+1});
+                
+            }
+            else{
+                if(!vis[i-10])q.push({i-10,level+1});
+                if(!vis[i+10])q.push({i+10,level+1});
+            }
+            int ch4=(i/powy[0])-(ch1*1000+ch2*100+ch3*10);
+
+            if(ch4==0){
+                if(!vis[i+1])q.push({i+1,level+1});  
+                if(!vis[i+9])q.push({i+9,level+1});
+            }
+            else if(ch4==9){
+                if(!vis[i-1])q.push({i-1,level+1});
+                if(!vis[i-9])q.push({i-9,level+1});                
+            }
+            else{
+                if(!vis[i-1])q.push({i-1,level+1});
+                if(!vis[i+1])q.push({i+1,level+1});         
             }
         }
         return -1;

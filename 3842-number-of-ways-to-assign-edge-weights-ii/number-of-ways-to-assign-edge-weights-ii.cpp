243512d39647e8d7a74e0d72925f25ae9680a98d
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-12 20:31:32 +0530

    Time: 446 ms (34.45%) | Memory: 388.3 MB (37.78%) - LeetSync

--- 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.cpp
+++ 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.cpp
@@ -1,113 +1,102 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    vector<vector<int>>adj;
-    vector<int>levels;
-    vector<int>parents;
-    vector<int>powy;
-    void findLevels(int node,int level,int par){
-        parents[node]=par;
-        levels[node]=level;
-        for(int i=0;i<adj[node].size();i++){
-            if(adj[node][i]!=par){
-                findLevels(adj[node][i],level+1,node);
-            }
+    static constexpr int MOD = 1e9 + 7;
+
+    vector<vector<int>> adj;
+    vector<vector<int>> up;
+    vector<int> depth;
+    vector<int> pow2;
+    int LOG;
+
+    void dfs(int node, int par) {
+        up[node][0] = par;
+
+        for (int j = 1; j < LOG; j++) {
+            if (up[node][j - 1] == -1)
+                up[node][j] = -1;
+            else
+                up[node][j] = up[up[node][j - 1]][j - 1];
+        }
+
+        for (int nxt : adj[node]) {
+            if (nxt == par) continue;
+            depth[nxt] = depth[node] + 1;
+            dfs(nxt, node);
         }
     }
-    vector<vector<int>>dp;
-    int createDp(int node,int j){
-        
-        if(j==0){
-            return dp[node][j]=parents[node];
+
+    int kthAncestor(int node, int k) {
+        for (int j = 0; j < LOG && node != -1; j++) {
+            if (k & (1 << j))
+                node = up[node][j];
         }
-        if(dp[node][j]!=-10){
-            return dp[node][j];
-        }
-        
-        return dp[node][j]=createDp(dp[node][j-1],j-1);
+        return node;
     }
-    int shift(int b,int k){
-        int i=0;
-        while(k>0){
-            if(k&1){
-                b=dp[b][i];
-            }
-            k/=2;
-            i++;
-        }
-        return b;
-    }
-    vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        powy.push_back(1);
-        int n=edges.size()+1;
-        for(int i=1;i<=1e5;i++){
-            powy.push_back((powy.back()*2)%mod);
-        }
-        int qn=queries.size();
-        adj.resize(n+1);
-        dp.resize(n+1,vector<int>(20,-10));
-        
-        levels.resize(n+1);
-        
-        parents.resize(n+1);
-        for(int i=0;i<n-1;i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
 
-        }
-        findLevels(1,0,-1);
+    int lca(int a, int b) {
+        if (depth[a] < depth[b]) swap(a, b);
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<20;j++){
-                
-                if(levels[i]<powy[j]){
-                    dp[i][j]=-1;
-                    continue;
-                }
-                if(dp[i][j]!=-10){
-                    continue;
-                }
-                createDp(i,j);
+        a = kthAncestor(a, depth[a] - depth[b]);
 
+        if (a == b) return a;
 
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (up[a][j] != up[b][j]) {
+                a = up[a][j];
+                b = up[b][j];
             }
         }
-        vector<int>ans;
-        for(int q=0;q<qn;q++){
-            int a=queries[q][0];
-            int b=queries[q][1];
-            if(a==b){
+
+        return up[a][0];
+    }
+
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges,
+                                  vector<vector<int>>& queries) {
+
+        int n = edges.size() + 1;
+
+        LOG = 1;
+        while ((1 << LOG) <= n) LOG++;
+
+        adj.assign(n + 1, {});
+        up.assign(n + 1, vector<int>(LOG, -1));
+        depth.assign(n + 1, 0);
+
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        dfs(1, -1);
+
+        int maxDepth = n + 5;
+
+        pow2.assign(maxDepth, 1);
+        for (int i = 1; i < maxDepth; i++) {
+            pow2[i] = (2LL * pow2[i - 1]) % MOD;
+        }
+
+        vector<int> ans;
+
+        for (auto &q : queries) {
+            int u = q[0];
+            int v = q[1];
+
+            if (u == v) {
                 ans.push_back(0);
                 continue;
             }
-            if(levels[a]>levels[b]){
-                swap(a,b);
-            }
-            int total=abs(levels[a]-levels[b]);
-            
-            b=shift(b,total);
-            if(a==b)ans.push_back(powy[total-1]);
-            else{
-                int lj=19;
-                while(lj>=0){
-                    if(dp[a][lj]!=dp[b][lj]){
-                        a=dp[a][lj];
-                        b=dp[b][lj];
-                        total+=2*powy[lj];
-                    }
-                    lj--;
-                }
-                total+=2;
-                ans.push_back(powy[total-1]);
 
-            }
+            int L = lca(u, v);
 
-            
-            
+            int dist = depth[u] + depth[v] - 2 * depth[L];
+
+            ans.push_back(pow2[dist - 1]);
         }
+
         return ans;
-        return {};
-
-
     }
 };

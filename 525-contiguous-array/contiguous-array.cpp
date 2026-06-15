754b2063115dc2506982a538a03a90cabbf3132e
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-15 15:59:43 +0530

    Time: 24 ms (56.80%) | Memory: 88.2 MB (93.45%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>v(n,vector<int>(2));
-        v[0][nums[0]]++;
-        unordered_map<int,int>mp;
-        mp[v[0][0]-v[0][1]]=0;
-        for(int i=1;i<n;i++){
-            v[i][0]=v[i-1][0];
-            v[i][1]=v[i-1][1];
-            v[i][nums[i]]++;
-            mp[v[i][0]-v[i][1]]=i;
-        }
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            if(i==0){
-                
-                if(mp[0]!=0)maxi=max(mp[0]+1,maxi);
+        unordered_map<int,int> first;
+        first[0] = -1;
+
+        int diff = 0;
+        int ans = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            diff += (nums[i] == 1 ? 1 : -1);
+
+            if(first.count(diff)) {
+                ans = max(ans, i - first[diff]);
             }
-            else{
-                int curr=v[i-1][0]-v[i-1][1];
-                if(mp[curr]!=i)maxi=max(maxi,mp[curr]-(i-1));
+            else {
+                first[diff] = i;
             }
         }
 
-        return maxi;
-        
-        
-        
+        return ans;
     }
-
-
-
-
-
-
 };

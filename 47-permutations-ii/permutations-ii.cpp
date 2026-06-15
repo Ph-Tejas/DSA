be0c3c32e956332986851f706f56cefcd4353cb8
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-16 02:56:49 +0530

    Time: 7 ms (31.04%) | Memory: 14.2 MB (23.81%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    map<int,int>mp;
+    int n;
+    void fun(vector<int>& nums,int i,vector<vector<int>>&ans,vector<int>v){
+        if(i==n){
+            ans.push_back(v);
+        }
+        for(auto &val:mp){
+            if(val.second>0){
+                val.second--;
+                v.push_back(val.first);
+                fun(nums,i+1,ans,v);
+                v.pop_back();
+                val.second++;
+            }
+        }
+
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n=nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        vector<vector<int>>ans;
+        fun(nums,0,ans,{});
+        return ans;
+    }
+};

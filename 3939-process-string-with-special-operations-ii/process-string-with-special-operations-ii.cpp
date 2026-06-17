a0804f8923ec27dd55a0c6b1ba068a3170689f40
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-17 16:51:06 +0530

    Time: 28 ms (60.92%) | Memory: 24.1 MB (28.74%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -3,11 +3,10 @@ public:
     char processStr(string s, long long k) {
         int n=s.size();
         long long szz=0;
+
         for(int i=0;i<n;i++){
             if(s[i]=='*'){
-                if(szz!=0){
-                    szz--;
-                }
+                if(szz) szz--;
             }
             else if(s[i]=='%'){
                 continue;
@@ -19,59 +18,40 @@ public:
                 szz++;
             }
         }
-        if(k>=szz){
-            return '.';
-        }
-        k++;
-        for(int i=0;i<26;i++){
-            long long sz=szz;
-            long long K=k;
-            char ch=i+'a';
-            
-            int it=n-1;
-            bool flag=false;
-            while(it>-1){
-                if(s[it]=='*'){
-                    sz++;
-                }
-                else if(s[it]=='%'){
-                    
-                    K=(sz-(K-1));
-                    
-                }
-                else if(s[it]=='#'){
-                    sz/=2;
-                    if(K>sz){
-                        K=K-(sz);
-                    }
-                    
-                }
-                else{
-                    if(sz==K){
-                        if(s[it]==ch){
-                            return ch;
-                        }
-                        else {
-                            flag=true;
-                            break;
-                        }
-                    }
-                    else{
-                        sz--;
-                    }
 
-                }
+        if(k>=szz) return '.';
 
+        long long K=k+1;
+        long long sz=szz;
 
+        int it=n-1;
 
-                it--;
+        while(it>=0){
+
+            if(s[it]=='*'){
+                sz++;
             }
-            
-            if(!flag && s[K-1]==ch){
-                return ch;
+            else if(s[it]=='%'){
+                K=sz-(K-1);
+            }
+            else if(s[it]=='#'){
+                sz/=2;
+                if(K>sz){
+                    K-=sz;
+                }
+            }
+            else{
+
+                if(sz==K){
+                    return s[it];
+                }
+
+                sz--;
             }
 
+            it--;
         }
-        return 'a';
+
+        return '.';
     }
 };

f4375659dd7b533a2df0242b0adb93a3140d239e
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-17 16:19:09 +0530

    Time: 139 ms (6.90%) | Memory: 24 MB (67.82%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        int n=s.size();
+        long long szz=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='*'){
+                if(szz!=0){
+                    szz--;
+                }
+            }
+            else if(s[i]=='%'){
+                continue;
+            }
+            else if(s[i]=='#'){
+                szz*=2;
+            }
+            else{
+                szz++;
+            }
+        }
+        if(k>=szz){
+            return '.';
+        }
+        k++;
+        for(int i=0;i<26;i++){
+            long long sz=szz;
+            long long K=k;
+            char ch=i+'a';
+            if(count(s.begin(),s.end(),ch)==0)continue;
+            cout<<ch;
+            int it=n-1;
+            bool flag=false;
+            while(it>-1){
+                if(s[it]=='*'){
+                    sz++;
+                }
+                else if(s[it]=='%'){
+                    
+                    K=(sz-(K-1));
+                    
+                }
+                else if(s[it]=='#'){
+                    sz/=2;
+                    if(K>sz){
+                        K=K-(sz);
+                    }
+                    
+                }
+                else{
+                    if(sz==K){
+                        if(s[it]==ch){
+                            return ch;
+                        }
+                        else {
+                            flag=true;
+                            break;
+                        }
+                    }
+                    else{
+                        sz--;
+                    }
+
+                }
+
+
+
+                it--;
+            }
+            
+            if(!flag && s[K-1]==ch){
+                return ch;
+            }
+
+        }
+        return 'a';
+    }
+};

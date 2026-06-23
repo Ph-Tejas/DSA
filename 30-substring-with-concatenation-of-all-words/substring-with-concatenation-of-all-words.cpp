1b23d962c587e6d73a3858ddaf30c910e5954ac5
Ph-Tejas <sharmatejas2005@gmail.com>
2026-06-23 22:09:35 +0530

    Time: 1041 ms (10.79%) | Memory: 135 MB (9.13%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -1,94 +1,84 @@
 class Solution {
 public:
+    // cout
     int eachn;
-    int mod = 1e9 + 7;
-    vector<long long> powers;
+    int mod=1e9+7;
+    vector<long long>powers;
 
-    long long powy(long long a, long long p) {
-        long long res = 1;
-
-        while (true) {
-            if (p & 1) {
-                res *= a;
-                res %= mod;
+    long long powy(long long a,long long p){
+        long long res=1;
+        while(true){
+            if(p&1){
+                res*=a;
                 p--;
-            } else {
-                if (p == 0) return res % mod;
-                a *= a;
-                a %= mod;
-                p /= 2;
+                res%=mod;
+            }
+            else{
+                if(p==0)return res%mod;
+                a*=a;
+                a%=mod;
+                p/=2;
             }
         }
-
-        return res % mod;
+        return res%mod;
     }
-
-    long long findHash(string &word) {
-        long long ans = 0;
-
-        for (int i = 0; i < word.size(); i++) {
-            ans += (powers[i] * ((word[i] - 'a') + 1)) % mod;
-            ans %= mod;
+    long long findHash(string &word){
+        long long ans=0;
+        int sz=word.size();
+        for(int i=0;i<sz;i++){
+            ans+=(powers[i]*((word[i]-'a')+1))%mod;
+            ans%=mod;
         }
-
         return ans;
     }
-
     vector<int> findSubstring(string s, vector<string>& words) {
+        int n=words.size();
+        
+        eachn=words[0].size();
+        if(((long long)eachn*n)>s.size())return {};
+        powers.resize(eachn+1);
+        int sz=s.size();
+        
+        map<int,int>nums;
+        for(int i=0;i<eachn+1;i++){
+            powers[i]=powy(27,i);
+            // cout<<powers[i]<<" ";
 
-        int n = words.size();
-        eachn = words[0].size();
-
-        if ((long long)s.size() < 1LL * n * eachn)
-            return {};
-
-        powers.resize(eachn + 1);
-
-        for (int i = 0; i <= eachn; i++) {
-            powers[i] = powy(27, i);
         }
-
-        map<long long, int> nums;
-
-        for (int i = 0; i < n; i++) {
-            nums[findHash(words[i])]++;
+        for(int i=0;i<n;i++){
+            nums[(findHash(words[i]))]++;
+            // cout<<findHash(words[i])<<" ";
         }
+        // cout<<endl;
 
-        int originalSize = s.size();
+        vector<int>ans;
+        string last;
+        long long initialHash=0;
+        if(eachn!=1)last=s.substr(sz-(eachn-1));
+        // cout<<last<<endl;
+        if(eachn!=1)initialHash=findHash(last);
+        // cout<<initialHash<<endl;
+        vector<int>snums(sz-eachn+1);
+        for(int i=sz-eachn;i>-1;i--){
+            initialHash*=27;
+            initialHash%=mod;
+            initialHash+=((s[i]-'a')+1);
 
-        string last = s.substr(originalSize - (eachn - 1));
+            initialHash%=mod;
+            snums[i]=initialHash;
+            initialHash=(initialHash-((long long)(((s[(i+eachn-1)]-'a')+1)*powers[eachn-1])%mod)+mod)%mod;
 
-        long long initialHash = findHash(last);
-
-        vector<long long> snums(originalSize - eachn + 1);
-
-        for (int i = originalSize - eachn; i >= 0; i--) {
-
-            initialHash *= 27;
-            initialHash %= mod;
-
-            initialHash += ((s[i] - 'a') + 1);
-            initialHash %= mod;
-
-            snums[i] = initialHash;
-
-            initialHash =
-                ((initialHash -
-                  (((long long)((s[i + eachn - 1] - 'a') + 1) *
-                    powers[eachn - 1]) %
-                   mod)) +
-                 mod) %
-                mod;
         }
+        sz=snums.size();
+        int originalSize=s.size();
 
-        vector<int> ans;
 
         int totalLen = n * eachn;
 
         for (int i = 0; i + totalLen <= originalSize; i++) {
 
             bool flag = true;
-            map<long long, int> mp = nums;
+            map<int, int> mp = nums;
 
             for (int j = i; j < i + totalLen; j += eachn) {
 
@@ -110,7 +100,12 @@ public:
                 ans.push_back(i);
             }
         }
-
         return ans;
+
+
+
+        
+
+        
     }
 };

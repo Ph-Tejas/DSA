90abefcc2bb7fa265fa1f9ff7a968065f46b879b
Ph-Tejas <sharmatejas2005@gmail.com>
2026-07-02 16:52:18 +0530

    Time: 314 ms (61.38%) | Memory: 19.1 MB (66.42%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -2,10 +2,10 @@ class Trie{
 public:
     Trie* collection[26];
     bool flag=false;
+
     Trie(){
         memset(collection,0,sizeof(collection));
     }
-
 };
 
 class Solution {
@@ -13,71 +13,74 @@ public:
     int n,m;
     bool vis[13][13];
 
-    vector<string>ans;
+    vector<string> ans;
     const int dir[4][2]={{0,1},{0,-1},{-1,0},{1,0}};
+
     void dfs(Trie* tr,string &s,int &sz,int idx){
         if(idx==sz){
-            tr->flag=true;  
-            return; 
+            tr->flag=true;
+            return;
         }
+
         int ch=s[idx]-'a';
-        if(tr->collection[ch]==NULL){
+
+        if(tr->collection[ch]==NULL)
             tr->collection[ch]=new Trie();
 
-        }
         dfs(tr->collection[ch],s,sz,idx+1);
     }
+
     void finding(Trie* tr,int i,int j,vector<vector<char>>& board,string &s){
-        if(vis[i][j])return;
-        vis[i][j]=true;
+
+        if(vis[i][j]) return;
+
         int ch=board[i][j]-'a';
 
-        s.push_back(ch+'a');
+        Trie* nxt=tr->collection[ch];
+        if(nxt==NULL) return;
 
-        if(tr->collection[ch]!=NULL){
-            if(tr->collection[ch]->flag==true){
-                ans.push_back(s);
-                tr->collection[ch]->flag=false;
+        vis[i][j]=true;
+        s.push_back(board[i][j]);
 
-            }
-            
-            for(int d=0;d<4;d++){
-                int ni=i+dir[d][0];
-                int nj=j+dir[d][1];
+        if(nxt->flag){
+            ans.push_back(s);
+            nxt->flag=false;
+        }
 
-                if(ni<n && nj<m && ni>-1 && nj>-1 && !vis[ni][nj]){
+        for(int d=0;d<4;d++){
+            int ni=i+dir[d][0];
+            int nj=j+dir[d][1];
 
-                    finding(tr->collection[ch],ni,nj,board,s);
-
-                }
+            if(ni>=0 && ni<n && nj>=0 && nj<m){
+                finding(nxt,ni,nj,board,s);
             }
         }
-        
-        
-        vis[i][j]=false;
+
         s.pop_back();
+        vis[i][j]=false;
     }
+
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+
         n=board.size();
         m=board[0].size();
+
         memset(vis,false,sizeof(vis));
+
         Trie* tr=new Trie();
-        
 
         for(auto &s:words){
             int sz=s.size();
             dfs(tr,s,sz,0);
-                
-            
         }
+
         for(int i=0;i<n;i++){
-            
             for(int j=0;j<m;j++){
                 string s;
                 finding(tr,i,j,board,s);
-
             }
         }
+
         return ans;
     }
 };

43c8a923bd1e1af403e13ec6f3116361b0162c36
Ph-Tejas <sharmatejas2005@gmail.com>
2026-07-03 20:23:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (86.52%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -5,14 +5,10 @@ public:
         
         int num,ct1,ct2;
         int ans=0;
-        ct1=0;
+        ct1=INT_MAX;
         ct2=0;
         for(int i=1;i<=n;i++){
             num=i;
-            while(num%2==0){
-                num/=2;
-                ct1++;
-            }
             while(num%5==0){
                 num/=5;
                 ct2++;

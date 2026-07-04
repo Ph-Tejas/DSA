1f7d9cee6d67081842b621656975a1b7868db999
Ph-Tejas <sharmatejas2005@gmail.com>
2026-07-04 19:12:51 +0530

    Time: 159 ms (5.53%) | Memory: 147.9 MB (18.78%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        map<int,int>mp;
+        mp[0]++;
+        nums[0]%=k;
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+            nums[i]%=k;
+            if(mp[nums[i]]>=1)return true;
+            mp[nums[i-1]]++;
+
+
+        }
+        return false;
+
+    }
+};

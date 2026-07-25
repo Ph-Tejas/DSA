0dca8281677d6f95bad54b3d36f6bf66c1093184
Ph-Tejas <sharmatejas2005@gmail.com>
2026-07-25 13:03:43 +0530

    Time: 5 ms (39.88%) | Memory: 30.2 MB (54.34%) - LeetSync

---
+++ 1705-count-unhappy-friends/count-unhappy-friends.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int ct=0;
+    vector<int>p;
+    void check(int a,int b,vector<vector<int>>&vmp,int &n,vector<vector<int>>& preferences){
+        for(auto &val:preferences[a]){
+            
+            if(val==b)break;
+            if(vmp[val][a]<vmp[val][p[val]]){
+                ct++;
+                break;
+            }
+
+        }
+    }
+    int unhappyFriends(int n, vector<vector<int>>& preferences, vector<vector<int>>& pairs) {
+        vector<vector<int>>vmp(n,vector<int>(n));
+        p.resize(n);
+
+        int sz=pairs.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1;j++){
+                vmp[i][preferences[i][j]]=j;
+            }
+        }
+        for(int i=0;i<sz;i++){
+            int a=pairs[i][0];
+            int b=pairs[i][1];
+            p[a]=b;
+            p[b]=a;
+        }
+        for(int i=0;i<sz;i++){
+            int a=pairs[i][0];
+            int b=pairs[i][1];
+            check(a,b,vmp,n,preferences);
+            check(b,a,vmp,n,preferences);
+            
+        }
+        return ct;
+
+
+
+    }
+};

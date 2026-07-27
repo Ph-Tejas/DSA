41d11f98e2a32ad1c32830f43f38a0c68552d551
Ph-Tejas <sharmatejas2005@gmail.com>
2026-07-27 20:27:41 +0530

    Time: 207 ms (5.05%) | Memory: 125.3 MB (5.05%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        vector<int>v;
+        v.push_back(0);
+        int n=heights.size();
+
+        for(int i=1;i<n;i++){
+            v.push_back(max(0,heights[i]-heights[i-1]));
+        }
+        int mini=0;
+        int maxi=n-1;
+        while(maxi>=mini){
+            int mid=mini+(maxi-mini)/2;
+            bool flag=false;
+            vector<int>v1;
+            for(int i=0;i<=mid;i++){
+                v1.push_back(v[i]);
+            }
+            int l=ladders;
+            sort(v1.begin(),v1.end());
+            if(l>=mid+1)flag=true;
+            while(!v1.empty() && l--){
+                v1.pop_back();
+            }
+            int sum=accumulate(v1.begin(),v1.end(),0);
+            if(sum<=bricks)flag=true;
+            if(flag)mini=mid+1;
+            else maxi=mid-1;
+        }
+        return maxi;
+    }
+};

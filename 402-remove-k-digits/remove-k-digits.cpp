67cae9bbe3a2a0146f71a713ae0d63d4e18ffc97
Ph-Tejas <sharmatejas2005@gmail.com>
2026-07-27 17:20:26 +0530

    Time: 10 ms (8.44%) | Memory: 23 MB (5.10%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n=num.size();
-        vector<vector<int>>v(n,vector<int>(10,10000000));
-        
-        for(int i=n-2;i>-1;i--){
-            v[i]=v[i+1];
-            v[i][num[i+1]-'0']=i+1;
+        int n = num.size();
+        const int INF = INT_MAX;
+
+        vector<array<int, 10>> v(n);
+        for (int j = 0; j < 10; j++)
+            v[n - 1][j] = INF;
+
+        for (int i = n - 2; i >= 0; i--) {
+            v[i] = v[i + 1];
+            v[i][num[i + 1] - '0'] = i + 1;
         }
+
         string fin;
-        for(int i=0;i<n;i++){
-            
-            for(int j=0;j<10;j++){
-                if(num[i]-'0'==j){
-                    fin.push_back(j+'0');
+        fin.reserve(n);
+
+        for (int i = 0; i < n; i++) {
+            int cur = num[i] - '0';
+
+            for (int j = 0; j < 10; j++) {
+                if (cur == j) {
+                    fin.push_back(char('0' + j));
                     break;
                 }
-                int loc=v[i][j];
-                if(loc-i<=k){
-                    k-=loc-i;
-                    i=loc;
-                    fin.push_back(j+'0');
+
+                int loc = v[i][j];
+                if (loc - i <= k) {
+                    k -= (loc - i);
+                    i = loc;
+                    fin.push_back(char('0' + j));
                     break;
                 }
             }
         }
-        while(k--){
-            fin.pop_back();
-        }
-        reverse(fin.begin(),fin.end());
-        while(!fin.empty()&&fin.back()=='0'){
-            fin.pop_back();
-        }
-        if(fin.empty())return "0";
-        reverse(fin.begin(),fin.end());
 
-        
-        return fin;
+        while (k--)
+            fin.pop_back();
+
+        int pos = 0;
+        while (pos < (int)fin.size() && fin[pos] == '0')
+            pos++;
+
+        if (pos == (int)fin.size())
+            return "0";
+
+        return fin.substr(pos);
     }
 };

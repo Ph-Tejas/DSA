7a8a8063575647df5e1ee80432e9119640ae728b
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-03 15:22:00 +0530

    Time: 7 ms (13.80%) | Memory: 115.8 MB (36.16%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -7,12 +7,13 @@ public:
             auto it=lower_bound(v.begin(),v.end(),nums[i]);
             if(it==v.end()){
                 v.push_back(nums[i]);
+
             }
             else{
                 v[it-v.begin()]=nums[i];
             }
+            if(v.size()==3)return true;
         }
-        if(v.size()>2)return true;
         return false;
     }
 };

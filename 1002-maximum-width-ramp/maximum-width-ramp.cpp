20ec5f6eda9b01d1767bce0b4621ee506d409d66
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-06 00:58:52 +0530

    Time: 768 ms (5.13%) | Memory: 183.6 MB (5.82%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v(1e5+1,-1);
+        vector<int>v2(1e5+1,-1);
+        for(int i=0;i<n;i++){
+            if(v[nums[i]]==-1)v[nums[i]]=i;
+            v2[nums[i]]=i;
+        }
+        vector<int>v1(1e5+1,-1);
+        int maxi=-1;
+        for(int i=1e5-1;i>-1;i--){
+            maxi=max(maxi,v2[i]);
+            v1[i]=maxi;
+        }
+        if(v1[0]==-1)return 0;
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            cout<<v1[i]<<" ";
+            int curr=nums[i];
+            ans=max(v1[nums[i]]-i,ans);
+        }
+        return ans;
+
+    }
+};

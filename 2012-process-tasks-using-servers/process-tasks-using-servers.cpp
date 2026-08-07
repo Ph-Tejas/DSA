f42acb057f77334e8aa767f5c5e94a85667ce4d7
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-07 22:27:05 +0530

    Time: 203 ms (27.89%) | Memory: 159.7 MB (25.50%) - LeetSync

--- 2012-process-tasks-using-servers/process-tasks-using-servers.cpp
+++ 2012-process-tasks-using-servers/process-tasks-using-servers.cpp
@@ -5,32 +5,32 @@ public:
         int m=tasks.size();
         vector<int>ans(m);
 
-        priority_queue<vector<long long>,vector<vector<long long>>,greater<vector<long long>>>pq1;
-        priority_queue<vector<long long>,vector<vector<long long>>,greater<vector<long long>>>pq2;
+        priority_queue<pair<long long,pair<long long,long long>>,vector<pair<long long,pair<long long,long long>>>,greater<pair<long long,pair<long long,long long>>>>pq1;
+        priority_queue<pair<long long,pair<long long,long long>>,vector<pair<long long,pair<long long,long long>>>,greater<pair<long long,pair<long long,long long>>>>pq2;
 
         for(int i=0;i<n;i++){
-            pq1.push({servers[i],i,0});
+            pq1.push({servers[i],{i,0}});
         }
         long long t=0;
         for(int i=0;i<m;i++){
             t=max(t,(long long)i);
             if(pq1.empty()){
-                t=max(pq2.top()[0],t);
+                t=max(pq2.top().first,t);
             }
             if(pq2.empty()){
-                int ind=pq1.top()[1];
-                int strength=pq1.top()[0];
+                int ind=pq1.top().second.first;
+                int strength=pq1.top().first;
                 ans[i]=ind;
                 // cout<<i<<" "<<tasks[ind]<<endl;
-                pq2.push({t+tasks[i],ind,strength});
+                pq2.push({t+tasks[i],{ind,strength}});
                 pq1.pop();
             }
             else{
-                // cout<<i<<" "<<pq2.top()[0]<<endl;
-                while(!pq2.empty() && t>=pq2.top()[0]){
+                // cout<<i<<" "<<pq2.top().first<<endl;
+                while(!pq2.empty() && t>=pq2.top().first){
                 
                     
-                    pq1.push({pq2.top()[2],pq2.top()[1],t});
+                    pq1.push({pq2.top().second.second,{pq2.top().second.first,t}});
                     
                     
                     pq2.pop();
@@ -38,10 +38,10 @@ public:
 
 
                 }
-                int ind=pq1.top()[1];
-                int strength=pq1.top()[0];
+                int ind=pq1.top().second.first;
+                int strength=pq1.top().first;
                 ans[i]=ind;
-                pq2.push({t+tasks[i],ind,strength});
+                pq2.push({t+tasks[i],{ind,strength}});
                 pq1.pop();
 
 

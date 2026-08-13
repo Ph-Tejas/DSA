b1a055a35588e60e43dada9402f766b2adb2bfb9
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-13 21:02:26 +0530

    Time: 163 ms (62.42%) | Memory: 76.9 MB (95.97%) - LeetSync

--- 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.cpp
+++ 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.cpp
@@ -1,136 +1,126 @@
-class Node{
+class Node {
 public:
-
+    int len;
     int maxLength;
     int pf;
     int sf;
     char pc;
     char sc;
-    Node(int maxLength,int pf,int sf,char pc,char sc){
-        this->maxLength=maxLength;
-        this->pf=pf;
-        this->sf=sf;
-        this->pc=pc;
-        this->sc=sc;
-        
-    } 
-    
 
+    Node() {}
+
+    Node(int len, int maxLength, int pf, int sf, char pc, char sc) {
+        this->len = len;
+        this->maxLength = maxLength;
+        this->pf = pf;
+        this->sf = sf;
+        this->pc = pc;
+        this->sc = sc;
+    }
 };
+
 class Solution {
 public:
-    
-    Node* segTree[400000];
 
-    bool intersectionSame(Node* &l,Node* &r){
-        return (l->sc)==(r->pc);
+    Node segTree[400005];
 
+    Node merge(Node &l, Node &r) {
+
+        Node res;
+
+        res.len = l.len + r.len;
+        res.pc = l.pc;
+        res.sc = r.sc;
+
+        res.maxLength = max(l.maxLength, r.maxLength);
+        res.pf = l.pf;
+        res.sf = r.sf;
+
+        if (l.sc == r.pc) {
+
+            res.maxLength = max(res.maxLength,
+                                l.sf + r.pf);
+
+            if (l.pf == l.len)
+                res.pf = l.len + r.pf;
+
+            if (r.sf == r.len)
+                res.sf = l.sf + r.len;
+        }
+
+        return res;
     }
-    Node* findFin(Node* l,Node* r,int i,int j,int mid){
-        int mx=max(l->maxLength,r->maxLength);
-        int sz,pz;
 
-        if(intersectionSame(l,r)){
-            
-            mx=max(mx,(l->sf)+(r->pf));
-            
+    void build(int i, int j, int ind, string &s) {
+
+        if (i == j) {
+            segTree[ind] = Node(
+                1,      // len
+                1,      // maxLength
+                1,      // pf
+                1,      // sf
+                s[i],   // pc
+                s[i]    // sc
+            );
+            return;
         }
-        if(j-(mid+1)+1==r->sf){
-            if(intersectionSame(l,r)){
-                sz=j-(mid+1)+1+l->sf;
-            }
-            else{
-                sz=r->sf;
-            }
+
+        int mid = i + (j - i) / 2;
+
+        build(i, mid, 2 * ind + 1, s);
+        build(mid + 1, j, 2 * ind + 2, s);
+
+        segTree[ind] = merge(
+            segTree[2 * ind + 1],
+            segTree[2 * ind + 2]
+        );
+    }
+
+    void update(int pos, char ch, int i, int j, int ind) {
+
+        if (i == j) {
+            segTree[ind] = Node(
+                1, 1, 1, 1, ch, ch
+            );
+            return;
         }
-        else{
-            sz=r->sf;
+
+        int mid = i + (j - i) / 2;
+
+        if (pos <= mid)
+            update(pos, ch, i, mid, 2 * ind + 1);
+        else
+            update(pos, ch, mid + 1, j, 2 * ind + 2);
+
+        segTree[ind] = merge(
+            segTree[2 * ind + 1],
+            segTree[2 * ind + 2]
+        );
+    }
+
+    vector<int> longestRepeating(
+        string s,
+        string queryCharacters,
+        vector<int>& queryIndices
+    ) {
+
+        int n = s.size();
+
+        build(0, n - 1, 0, s);
+
+        vector<int> ans;
+
+        for (int i = 0; i < queryCharacters.size(); i++) {
+
+            update(
+                queryIndices[i],
+                queryCharacters[i],
+                0, n - 1, 0
+            );
+
+            ans.push_back(segTree[0].maxLength);
         }
-        if(mid-i+1==l->pf){
-            if(intersectionSame(l,r)){
-                pz=mid-i+1+r->pf;
-            }
-            else{
-                pz=l->pf;
-            }
-        }
-        else pz=l->pf;
-        char chp=l->pc;
-        char chs=r->sc;
-        Node* ans=new Node(mx,pz,sz,chp,chs);
+
         return ans;
-    }
-
-    Node* build(int i,int j,string &s,int ind){
-        if(i==j){
-            segTree[ind]=new Node(1,1,1,s[i],s[j]);
-            return segTree[ind];
-        }
-        int mid=i+(j-i)/2;
-        Node* l=build(i,mid,s,2*ind+1);
-        Node* r=build(mid+1,j,s,2*ind+2);
-
-
-        return segTree[ind]=findFin(l,r,i,j,mid);
-
-
-    }
-
-
-    Node* update(int loc,int ch,int i,int j,int ind,string &s){
-        
-        if(i>loc||j<loc)return segTree[ind];
-        if(i==j){
-            segTree[ind]->sc=ch;
-            segTree[ind]->pc=ch;
-            return segTree[ind];
-        }
-        int mid=i+(j-i)/2;
-        Node* l=update(loc,ch,i,mid,2*ind+1,s);
-        Node* r=update(loc,ch,mid+1,j,2*ind+2,s);
-
-   
-
-        return segTree[ind]=findFin(l,r,i,j,mid);
-
-
-        
-
-    }
-
-
-
-    vector<int> longestRepeating(string s, string queryCharacters, vector<int>& queryIndices) {
-        int n=s.size();
-        int m=queryCharacters.size();
-        memset(segTree,NULL,sizeof(segTree));
-
-        build(0,n-1,s,0);
-        
-        vector<int>ans(m);
-        for(int i=0;i<m;i++){
-            int ch=queryCharacters[i];
-            int loc=queryIndices[i];
-
-
-
-            Node* res=update(loc,ch,0,n-1,0,s);
-            
-            ans[i]=res->maxLength;
-
-        }
-        return ans;
-
-
-
-
-
-
-
-
-
-
-
-
     }
 };

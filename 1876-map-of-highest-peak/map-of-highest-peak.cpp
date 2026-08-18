2f37311249df899fe931003801a564e166bbbdef
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-18 20:48:15 +0530

    Time: 136 ms (9.03%) | Memory: 145.2 MB (8.68%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -6,18 +6,18 @@ public:
 
 
         vector<vector<int>>vis(n,vector<int>(m,-1));
-        queue<vector<int>>q;
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(isWater[i][j])q.push({0,i,j});
+                if(isWater[i][j])q.push({{i,j},0});
             }
         }
         vector<vector<int>>dir={{0,1},{0,-1},{1,0},{-1,0}};
 
         while(!q.empty()){
-            int h=q.front()[0];
-            int i=q.front()[1];
-            int j=q.front()[2];
+            int h=q.front().second;
+            int i=q.front().first.first;
+            int j=q.front().first.second;
             
             q.pop();    
             if(vis[i][j]!=-1)continue;
@@ -27,7 +27,7 @@ public:
                 int ni=dir[c][0]+i;
                 int nj=dir[c][1]+j;
                 if(ni>=0 && nj>=0 && ni<n &&nj<m && vis[ni][nj]==-1){
-                    q.push({h+1,ni,nj});
+                    q.push({{ni,nj},h+1});
                 }
                 
             }

cd7415df84586c008005078c233d7bf59c4c7e98
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-19 05:40:37 +0530

    Time: 102 ms (5.11%) | Memory: 110.8 MB (5.11%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,31 +1,53 @@
 class Solution {
 public:
     vector<vector<int>>adj;
+    vector<int>indeg;
+
+    void dfs(int node){
+        for(auto &val:adj[node]){
+            indeg[val]--;
+            if(indeg[val]==0)dfs(val);
+        }
+    }
+    
     int maximumJumps(vector<int>& nums, int target) {
         int n=nums.size();
         adj.resize(n);
+        indeg.resize(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 
-                if(abs(nums[j]-nums[i])<=target)adj[i].push_back(j);
+                if(abs(nums[j]-nums[i])<=target){
+                    adj[i].push_back(j);
+                    indeg[j]++;
+                }
             }
         }
         vector<int>vis(n,-1);
-        priority_queue<pair<int,int>>q;
+        queue<pair<int,int>>q;
         q.push({0,0});
+        vector<int>v;
+        for(int i=1;i<n;i++){
+            if(indeg[i]==0)v.push_back(i);
+        }
+
+        for(auto &val:v){
+            dfs(val);
+        }
         
         while(!q.empty()){
-            int node=q.top().second;
-            int c=q.top().first;
+            int node=q.front().second;
+            int c=q.front().first;
             q.pop();
-            
-            if(vis[node]>=c)continue;
             vis[node]=c;
+
+
             
 
             for(auto &val:adj[node]){
-                if(vis[val]>=c+1)continue;
-                q.push({c+1,val});
+                
+                indeg[val]--;
+                if(indeg[val]==0)q.push({c+1,val});
             }
         }
 

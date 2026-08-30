3c9ceb8529e54aedb7fc7c1369c521120249254a
Ph-Tejas <sharmatejas2005@gmail.com>
2026-08-30 17:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 92.1 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -5,8 +5,7 @@ public:
         int it1=max_element(nums.begin(),nums.end())-nums.begin();
         int it2=min_element(nums.begin(),nums.end())-nums.begin();
         if(it1>it2)swap(it1,it2);
-        int ans=min(it2+1,n-it1);
-        return min(ans,it1+1+n-it2);
+        return min({it1+1+n-it2,it2+1,n-it1});
         
     }
 };

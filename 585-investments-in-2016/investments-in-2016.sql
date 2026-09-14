5e000a4a40bcdbd0717f84435b09c6684ee5b236
Ph-Tejas <sharmatejas2005@gmail.com>
2026-09-14 21:37:05 +0530

    Time: 705 ms (33.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select round(sum(c.tiv_2016),2) as tiv_2016 from (select * from insurance group by lat,lon having count(*)<2)as a inner JOIN (select * from insurance where (tiv_2015) in(select tiv_2015 from insurance group by tiv_2015 having count(*)>1) ) as c on a.pid=c.pid ;

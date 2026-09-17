28445e88614c0b9043667d13e0f5dd6cc8931b99
Ph-Tejas <sharmatejas2005@gmail.com>
2026-09-17 21:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.77%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st=[]
+        n=len(s)
+
+        for i in range(n):
+            if s[i]=='}' or s[i]==')' or s[i]==']':
+                if len(st)==0:
+                    return False
+                
+                if s[i]=='}':
+                    if st[-1]=='{':
+                        st.pop()
+                    
+                    else:
+                        return False
+
+
+                if s[i]==')':
+                    if st[-1]=='(':
+                        st.pop()
+                    
+                    else:
+                        return False
+
+                if s[i]==']':
+                    if st[-1]=='[':
+                        st.pop()
+                    
+                    else:
+                        return False
+
+            else:
+                st.append(s[i])
+
+        if len(st)!=0:
+            return False
+
+        else:
+            return True
+
+                
+
+        

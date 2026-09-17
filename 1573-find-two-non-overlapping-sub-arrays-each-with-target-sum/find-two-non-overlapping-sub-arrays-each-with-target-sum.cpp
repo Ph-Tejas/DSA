69c203ed6b3539402b18b7c3655ec6b11fdf6ecd
Ph-Tejas <sharmatejas2005@gmail.com>
2026-09-17 20:31:35 +0530

    Time: 1507 ms (5.16%) | Memory: 100.5 MB (38.40%) - LeetSync

---
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& arr, int target) {
+        int n=arr.size();
+        vector<int>pf(n+1);
+        for(int i=1;i<=n;i++){
+            pf[i]=pf[i-1]+arr[i-1];
+        }
+        vector<int>tgt;
+        for(int i=1;i<=n;i++){
+           
+            auto it=lower_bound(pf.begin(),pf.end(),pf[i-1]+target);
+
+            if(it!=pf.end() && (*it)==pf[i-1]+target){
+                tgt.push_back((it-pf.begin())-1);
+            }
+            else{
+                tgt.push_back(-1);
+            }
+        }   
+        for(int i=0;i<n;i++){
+            if(tgt[i]!=-1){
+                tgt[i]=(tgt[i]-i+1);
+            }
+            cout<<tgt[i]<<" ";
+        }
+        cout<<endl;
+        vector<int>tgt_s=tgt;
+        int ans=INT_MAX;
+        for(int i=n-2;i>-1;i--){
+            if(tgt_s[i]==-1){
+                tgt_s[i]=tgt_s[i+1];
+            }
+            else{
+                if(tgt_s[i+1]!=-1)tgt_s[i]=min(tgt_s[i],tgt_s[i+1]);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(tgt[i]!=-1){
+                if(i+tgt[i]!=n){
+                    if(tgt_s[i+tgt[i]]!=-1){
+                        ans=min(ans,tgt[i]+tgt_s[i+tgt[i]]);
+                    }
+                }
+            }
+        }
+        return ((ans==INT_MAX)?-1:ans);
+
+        
+    }
+};
